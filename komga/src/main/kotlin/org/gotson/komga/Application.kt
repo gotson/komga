3751522b1c3c66e761584e65916625f77dad35bf
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2019-08-18 13:38:09 +0800

    don't load scheduling during tests

--- komga/src/main/kotlin/org/gotson/komga/Application.kt
+++ komga/src/main/kotlin/org/gotson/komga/Application.kt
@@ -4,10 +4,11 @@ import org.gotson.komga.infrastructure.configuration.KomgaProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
-@EnableScheduling
 @EnableConfigurationProperties(
     KomgaProperties::class
 )
@@ -16,3 +17,8 @@ class Application
 fun main(args: Array<String>) {
   runApplication<Application>(*args)
 }
+
+@Profile("!test")
+@Configuration
+@EnableScheduling
+class SchedulingConfiguration
