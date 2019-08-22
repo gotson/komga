d81d3f797b4a361bb19e76a40ac450945621ed79
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2019-08-22 13:55:36 +0800

    add test to validate thumbnails are using the correct file (failing - reproducing issue faced in prod)

---
+++ komga/src/test/kotlin/org/gotson/komga/domain/service/BookParserTest.kt
@@ -0,0 +1,39 @@
+package org.gotson.komga.domain.service
+
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.slot
+import org.assertj.core.api.Assertions.assertThat
+import org.gotson.komga.domain.model.makeBook
+import org.gotson.komga.domain.model.makeBookPage
+import org.gotson.komga.domain.model.path
+import org.gotson.komga.infrastructure.archive.ContentDetector
+import org.gotson.komga.infrastructure.archive.RarExtractor
+import org.gotson.komga.infrastructure.archive.ZipExtractor
+import org.junit.jupiter.api.Test
+
+class BookParserTest {
+  private val mockContent = mockk<ContentDetector>()
+  private val mockZip = mockk<ZipExtractor>()
+  private val mockRar = mockk<RarExtractor>()
+
+  private val bookParser = BookParser(mockContent, mockZip, mockRar)
+
+  @Test
+  fun `given book with unordered pages when parsing then thumbnail should always be the first in natural order`() {
+    // given
+    val book = makeBook("book")
+    every { mockContent.detectMediaType(book.path()) } returns "application/zip"
+
+    val unorderedPages = listOf("08", "01", "02").map { makeBookPage(it) }
+    every { mockZip.getPagesList(book.path()) } returns unorderedPages
+
+    //when
+    val thumbnailFile = slot<String>()
+    every { mockZip.getPageStream(book.path(), capture(thumbnailFile)) } returns ByteArray(1)
+    val metadata = bookParser.parse(book)
+
+    // then
+    assertThat(thumbnailFile.captured).isEqualTo("01")
+  }
+}
