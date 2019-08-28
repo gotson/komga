3d2d31e9a5b48a9e52dbac8cc10bbb67d6ef476b
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2019-08-28 22:10:35 +0800

    removed directJpeg as it's useless fix mediaType guessing for other formats

--- komga/src/main/kotlin/org/gotson/komga/infrastructure/archive/PdfExtractor.kt
+++ komga/src/main/kotlin/org/gotson/komga/infrastructure/archive/PdfExtractor.kt
@@ -40,21 +40,9 @@ class PdfExtractor(
       COSName.DCT_DECODE.name,
       COSName.DCT_DECODE_ABBREVIATION.name
   )
-  private val directJPEG = true
 
   override fun getPagesList(path: Path): List<BookPage> {
     return PDDocument.load(Files.newInputStream(path)).use { pdf ->
-      //      val xobjs = pdf.pages.flatMap { page ->
-//        page.resources.xObjectNames.map { cos ->
-//          cos.name to page.resources.getXObject(cos)
-//        }
-//      }.toMap()
-//
-//      xobjs.mapNotNull { (entry, xobj) ->
-//        if (xobj is PDImageXObject) {
-//          BookPage(entry, "image/png")
-//        } else null
-//      }
       pdf.pages.mapIndexed { index, page ->
         val extractor = ImageGraphicsEngine(page, false)
         extractor.run()
@@ -64,17 +52,6 @@ class PdfExtractor(
   }
 
   override fun getPageStream(path: Path, entryName: String): ByteArray {
-//    return PDDocument.load(Files.newInputStream(path)).use { pdf ->
-//      val xobjs = pdf.pages.flatMap { page ->
-//        page.resources.xObjectNames.map { cos ->
-//          cos.name to page.resources.getXObject(cos)
-//        }
-//      }.toMap()
-//
-//      val buffer = ByteArrayOutputStream()
-//      ImageIO.write((xobjs[entryName] as PDImageXObject).image, "png", buffer)
-//      buffer.toByteArray()
-//    }
     return PDDocument.load(Files.newInputStream(path)).use { pdf ->
       val extractor = ImageGraphicsEngine(pdf.pages[entryName.toInt()], true)
       extractor.run()
@@ -99,9 +76,6 @@ class PdfExtractor(
         if (softMask != null) {
           val group = softMask.group
           if (group != null) {
-            // PDFBOX-4327: without this line NPEs will occur
-//            res.getExtGState(name).copyIntoGraphicsState(graphicsState)
-
             processSoftMask(group)
           }
         }
@@ -124,18 +98,8 @@ class PdfExtractor(
         if (pdImage.isStencil()) {
           processColor(graphicsState.nonStrokingColor)
         }
-//        if (seen.contains(pdImage.cosObject)) {
-//          // skip duplicate image
-//          return
-//        }
-//        seen.add(pdImage.cosObject)
       }
-
-      // save image
-//      val name = prefix + "-" + imageCounter
-//      imageCounter++
-
-      writeToBuffer(pdImage, directJPEG, extractImage)
+      writeToBuffer(pdImage, extractImage)
     }
 
     override fun fillAndStrokePath(windingRule: Int) {
@@ -174,7 +138,7 @@ class PdfExtractor(
     override fun appendRectangle(p0: Point2D?, p1: Point2D?, p2: Point2D?, p3: Point2D?) {}
     override fun curveTo(x1: Float, y1: Float, x2: Float, y2: Float, x3: Float, y3: Float) {}
 
-    private fun writeToBuffer(pdImage: PDImage, directJPEG: Boolean, extractImage: Boolean) {
+    private fun writeToBuffer(pdImage: PDImage, extractImage: Boolean) {
       var suffix: String? = pdImage.suffix
       logger.trace { "PDF image suffix: $suffix" }
       if (suffix == null || suffix == "jb2") {
@@ -192,7 +156,7 @@ class PdfExtractor(
             "jpg" -> {
               mediaType = "image/jpeg"
               val colorSpaceName = pdImage.colorSpace.name
-              if (directJPEG || !hasMasks(pdImage) && (PDDeviceGray.INSTANCE.name == colorSpaceName || PDDeviceRGB.INSTANCE.name == colorSpaceName)) {
+              if (!hasMasks(pdImage) && (PDDeviceGray.INSTANCE.name == colorSpaceName || PDDeviceRGB.INSTANCE.name == colorSpaceName)) {
                 // RGB or Gray colorspace: get and write the unmodified JPEG stream
                 if (extractImage) {
                   logger.debug { "RGB or Gray colorspace, get the unmodified JPEG stream" }
@@ -202,7 +166,6 @@ class PdfExtractor(
                 }
               } else {
                 // for CMYK and other "unusual" colorspaces, the JPEG will be converted
-                //            ImageIOUtil.writeImage(image, suffix, out)
                 if (extractImage) {
                   logger.debug { "CMYK or other colorspace, converting to JPEG" }
                   ImageIO.write(image, suffix, out)
@@ -212,7 +175,7 @@ class PdfExtractor(
             "jp2" -> {
               mediaType = "image/jp2"
               val colorSpaceName = pdImage.colorSpace.name
-              if (directJPEG || !hasMasks(pdImage) && (PDDeviceGray.INSTANCE.name == colorSpaceName || PDDeviceRGB.INSTANCE.name == colorSpaceName)) {
+              if (!hasMasks(pdImage) && (PDDeviceGray.INSTANCE.name == colorSpaceName || PDDeviceRGB.INSTANCE.name == colorSpaceName)) {
                 // RGB or Gray colorspace: get and write the unmodified JPEG2000 stream
                 if (extractImage) {
                   logger.debug { "RGB or Gray colorspace, get the unmodified JPEG2000 stream" }
@@ -230,9 +193,8 @@ class PdfExtractor(
               }
             }
             else -> {
-              mediaType = URLConnection.guessContentTypeFromName("file$suffix")
+              mediaType = URLConnection.guessContentTypeFromName("file.$suffix") ?: "application/octet-stream"
               if (extractImage) {
-                //          ImageIOUtil.writeImage(image, suffix, out)
                 logger.debug { "Converting to $suffix" }
                 ImageIO.write(image, suffix, out)
               }
