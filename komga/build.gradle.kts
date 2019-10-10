9df64ee2cf923415e46ccdba65c564baeb592ddd
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2019-10-10 14:22:45 +0800

    update gradle dependencies remove dependency version for dependencies included with Spring Boot

--- komga/build.gradle.kts
+++ komga/build.gradle.kts
@@ -11,9 +11,9 @@ plugins {
     kotlin("plugin.jpa") version kotlinVersion
     kotlin("kapt") version kotlinVersion
   }
-  id("org.springframework.boot") version "2.1.7.RELEASE"
+  id("org.springframework.boot") version "2.1.9.RELEASE"
   id("io.spring.dependency-management") version "1.0.8.RELEASE"
-  id("com.github.ben-manes.versions") version "0.24.0"
+  id("com.github.ben-manes.versions") version "0.25.0"
   id("com.palantir.docker") version "0.22.1"
   id("com.github.breadmoirai.github-release") version "2.2.9"
   jacoco
@@ -34,7 +34,7 @@ repositories {
 dependencies {
   implementation(kotlin("stdlib-jdk8"))
   implementation(kotlin("reflect"))
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.0")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.2")
 
   implementation("org.springframework.boot:spring-boot-starter-web")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
@@ -44,11 +44,11 @@ dependencies {
 
   kapt("org.springframework.boot:spring-boot-configuration-processor")
 
-  implementation("org.flywaydb:flyway-core:5.2.4")
+  implementation("org.flywaydb:flyway-core")
 
 //  implementation("com.github.ben-manes.caffeine:caffeine:2.7.0")
 
-  implementation("io.github.microutils:kotlin-logging:1.6.26")
+  implementation("io.github.microutils:kotlin-logging:1.7.6")
 
   run {
     val springfoxVersion = "2.9.2"
@@ -56,18 +56,17 @@ dependencies {
     implementation("io.springfox:springfox-swagger-ui:$springfoxVersion")
   }
 
-  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.9")
-  implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.9.9")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+  implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml")
 
   implementation("com.github.klinq:klinq-jpaspec:0.8")
 
   implementation("commons-io:commons-io:2.6")
-
   implementation("org.apache.commons:commons-lang3:3.9")
 
   implementation("org.apache.tika:tika-core:1.22")
   implementation("com.github.junrar:junrar:4.0.0")
-  implementation("org.apache.pdfbox:pdfbox:2.0.16")
+  implementation("org.apache.pdfbox:pdfbox:2.0.17")
   implementation("net.grey-panther:natural-comparator:1.1")
 
   implementation("net.coobird:thumbnailator:0.4.8")
@@ -75,15 +74,15 @@ dependencies {
   implementation("com.twelvemonkeys.imageio:imageio-tiff:3.4.2")
   implementation(files("$projectDir/libs/webp-imageio-decoder-plugin-0.2.jar"))
 
-  runtimeOnly("com.h2database:h2:1.4.199")
+  runtimeOnly("com.h2database:h2")
 
   testImplementation("org.springframework.boot:spring-boot-starter-test") {
     exclude(module = "junit")
     exclude(module = "mockito-core")
   }
   testImplementation("org.springframework.security:spring-security-test")
-  testImplementation("org.junit.jupiter:junit-jupiter:5.5.0")
-  testImplementation("com.ninja-squad:springmockk:1.1.2")
+  testImplementation("org.junit.jupiter:junit-jupiter-engine")
+  testImplementation("com.ninja-squad:springmockk:1.1.3")
   testImplementation("io.mockk:mockk:1.9.3")
   testImplementation("com.google.jimfs:jimfs:1.1")
 
