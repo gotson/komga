476de466bf1716236076ed47cefd58daaac7a296
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2019-10-21 17:36:22 +0800

    remove unused dependency

--- komga/build.gradle.kts
+++ komga/build.gradle.kts
@@ -39,7 +39,7 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-web")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-actuator")
-  implementation("org.springframework.boot:spring-boot-starter-cache")
+//  implementation("org.springframework.boot:spring-boot-starter-cache")
   implementation("org.springframework.boot:spring-boot-starter-security")
 
   kapt("org.springframework.boot:spring-boot-configuration-processor")
