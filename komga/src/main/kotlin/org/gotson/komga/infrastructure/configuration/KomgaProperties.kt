e503156bfd0d943a1991c7eb67ad151ab011475e
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2019-11-15 15:47:03 +0800

    remove deprecated configuration properties

--- komga/src/main/kotlin/org/gotson/komga/infrastructure/configuration/KomgaProperties.kt
+++ komga/src/main/kotlin/org/gotson/komga/infrastructure/configuration/KomgaProperties.kt
@@ -1,7 +1,6 @@
 package org.gotson.komga.infrastructure.configuration
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.context.properties.DeprecatedConfigurationProperty
 import org.springframework.stereotype.Component
 import org.springframework.validation.annotation.Validated
 import javax.validation.constraints.Min
@@ -10,28 +9,9 @@ import javax.validation.constraints.Min
 @ConfigurationProperties(prefix = "komga")
 @Validated
 class KomgaProperties {
-  @get:DeprecatedConfigurationProperty(reason = "As of v0.5.0 Komga supports multiple libraries, which must be created via the API")
-  var rootFolder: String = ""
-
-  @get:DeprecatedConfigurationProperty(
-      reason = "As of v0.5.0 Komga supports multiple libraries, which must be created via the API",
-      replacement = "komga.libraries-scan-cron"
-  )
-  var rootFolderScanCron: String = ""
-
   var librariesScanCron: String = ""
-    get() {
-      if (field.isBlank()) return rootFolderScanCron
-      return field
-    }
 
   var librariesScanDirectoryExclusions: List<String> = emptyList()
-
-  @get:DeprecatedConfigurationProperty(reason = "As of v0.7.0 Komga supports user creation via the API/UI")
-  var userPassword: String = ""
-
-  @get:DeprecatedConfigurationProperty(reason = "As of v0.7.0 Komga supports user creation via the API/UI")
-  var adminPassword: String = ""
 
   var threads = Threads()
 
