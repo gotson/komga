gotson/komga

commit bb183828a189ce5908d21304bbf1592ae074afc5

fix: webui works with baseUrl

komga/src/main/kotlin/org/gotson/komga/infrastructure/web/StaticResourceConfiguration.kt
@@ -1,22 +1,42 @@
 package org.gotson.komga.infrastructure.web
 
+import org.springframework.boot.web.server.ErrorPage
+import org.springframework.boot.web.server.WebServerFactoryCustomizer
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.CacheControl
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Component
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
 
 @Configuration
 class StaticResourceConfiguration : WebMvcConfigurer {
   override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
     registry//.setOrder(Ordered.HIGHEST_PRECEDENCE)
-        .addResourceHandler(
-            "/index.html",
-            "/favicon.ico"
-        )
-        .addResourceLocations(
-            "classpath:public/index.html",
-            "classpath:public/favicon.ico"
-        )
-        .setCacheControl(CacheControl.noStore())
+      .addResourceHandler(
+        "/index.html",
+        "/favicon.ico"
+      )
+      .addResourceLocations(
+        "classpath:public/index.html",
+        "classpath:public/favicon.ico"
+      )
+      .setCacheControl(CacheControl.noStore())
+  }
+
+  override fun addViewControllers(registry: ViewControllerRegistry) {
+    registry.addViewController("/notFound")
+      .setStatusCode(HttpStatus.OK)
+      .setViewName("forward:/")
+  }
+}
+
+@Component
+class CustomContainer : WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
+  override fun customize(factory: ConfigurableServletWebServerFactory) {
+    factory.addErrorPages(ErrorPage(HttpStatus.NOT_FOUND, "/notFound"))
   }
 }

komga/src/main/resources/application.yml
@@ -22,7 +22,8 @@ spring:
         use_second_level_cache: true
         use_query_cache: true
         region.factory_class: org.hibernate.cache.jcache.JCacheRegionFactory
-
+  thymeleaf:
+    prefix: classpath:/public/
 server.servlet.session.timeout: 7d
 
 management:

komga-webui/public/index.html
@@ -4,8 +4,13 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
-    <link rel="icon" href="<%= BASE_URL %>favicon.ico">
     <title>Komga</title>
+    <link rel="icon" href="/favicon.ico" th:href="@{/favicon.ico}">
+    <script th:inline="javascript">
+        /*<![CDATA[*/
+        window.resourceBaseUrl = /*[(<%="$"%>{"'" + baseUrl + "'"})]*/ '/'
+        /*]]>*/
+    </script>
 </head>
 <body>
 <noscript>

komga-webui/src/functions/urls.ts
@@ -1,15 +1,27 @@
-const baseURL = process.env.VUE_APP_KOMGA_API_URL ? process.env.VUE_APP_KOMGA_API_URL : window.location.origin
+const fullUrl = process.env.VUE_APP_KOMGA_API_URL
+  ? process.env.VUE_APP_KOMGA_API_URL
+  : window.location.origin + window.resourceBaseUrl
+const baseUrl = process.env.NODE_ENV === 'production' ? window.resourceBaseUrl : '/'
+
+const urls = {
+  origin: !fullUrl.endsWith('/') ? `${fullUrl}/` : fullUrl,
+  originNoSlash: fullUrl.endsWith('/') ? fullUrl.slice(0, -1) : fullUrl,
+  base: !baseUrl.endsWith('/') ? `${baseUrl}/` : baseUrl,
+  baseNoSlash: baseUrl.endsWith('/') ? baseUrl.slice(0, -1) : baseUrl
+} as Urls
+
+export default urls
 
 export function bookThumbnailUrl (bookId: number): string {
-  return `${baseURL}/api/v1/books/${bookId}/thumbnail`
+  return `${urls.originNoSlash}/api/v1/books/${bookId}/thumbnail`
 }
 
 export function bookFileUrl (bookId: number): string {
-  return `${baseURL}/api/v1/books/${bookId}/file`
+  return `${urls.originNoSlash}/api/v1/books/${bookId}/file`
 }
 
 export function bookPageUrl (bookId: number, page: number, convertTo?: string): string {
-  let url = `${baseURL}/api/v1/books/${bookId}/pages/${page}`
+  let url = `${urls.originNoSlash}/api/v1/books/${bookId}/pages/${page}`
   if (convertTo) {
     url += `?convert=${convertTo}`
   }
@@ -17,9 +29,9 @@ export function bookPageUrl (bookId: number, page: number, convertTo?: string): 
 }
 
 export function bookPageThumbnailUrl (bookId: number, page: number): string {
-  return `${baseURL}/api/v1/books/${bookId}/pages/${page}/thumbnail`
+  return `${urls.originNoSlash}/api/v1/books/${bookId}/pages/${page}/thumbnail`
 }
 
 export function seriesThumbnailUrl (seriesId: number): string {
-  return `${baseURL}/api/v1/series/${seriesId}/thumbnail`
+  return `${urls.originNoSlash}/api/v1/series/${seriesId}/thumbnail`
 }

komga-webui/src/main.ts
@@ -1,3 +1,4 @@
+import './public-path'
 import _, { LoDashStatic } from 'lodash'
 import Vue from 'vue'
 import VueCookies from 'vue-cookies'
@@ -45,5 +46,11 @@ new Vue({
 declare module 'vue/types/vue' {
   interface Vue {
     $_: LoDashStatic;
+  }
+}
+
+declare global {
+  interface Window {
+    resourceBaseUrl: string
   }
 }

komga-webui/src/plugins/http.plugin.ts
@@ -1,10 +1,11 @@
+import urls from '@/functions/urls'
 import axios, { AxiosInstance, AxiosRequestConfig } from 'axios'
 import _Vue from 'vue'
 
 export default {
   install (Vue: typeof _Vue) {
     Vue.prototype.$http = axios.create({
-      baseURL: process.env.VUE_APP_KOMGA_API_URL ? process.env.VUE_APP_KOMGA_API_URL : window.location.origin,
+      baseURL: urls.origin,
       withCredentials: true,
       headers: { 'X-Requested-With': 'XMLHttpRequest' }
     } as AxiosRequestConfig)

komga-webui/src/public-path.js
@@ -1,0 +1,2 @@
+// eslint-disable-next-line camelcase
+__webpack_public_path__ = process.env.NODE_ENV === 'production' ? window.location.origin + window.resourceBaseUrl : '/'

komga-webui/src/router.ts
@@ -1,3 +1,4 @@
+import urls from '@/functions/urls'
 import Vue from 'vue'
 import Router from 'vue-router'
 import store from './store'
@@ -13,7 +14,7 @@ const adminGuard = (to: any, from: any, next: any) => {
 
 const router = new Router({
   mode: 'history',
-  base: process.env.BASE_URL,
+  base: urls.base,
   routes: [
     {
       path: '/',

commit fb147a447a4ba8aff73da57fff1c511a9d515b20

fix: send proper JSON on API 404 instead of index.html
bug introduced in bb183828a189ce5908d21304bbf1592ae074afc5

komga/src/main/kotlin/org/gotson/komga/infrastructure/web/StaticResourceConfiguration.kt
@@ -1,15 +1,14 @@
 package org.gotson.komga.infrastructure.web
 
-import org.springframework.boot.web.server.ErrorPage
-import org.springframework.boot.web.server.WebServerFactoryCustomizer
-import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.CacheControl
-import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.servlet.NoHandlerFoundException
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import java.util.concurrent.TimeUnit
 
 
 @Configuration
@@ -25,18 +24,29 @@ class StaticResourceConfiguration : WebMvcConfigurer {
         "classpath:public/favicon.ico"
       )
       .setCacheControl(CacheControl.noStore())
-  }
 
-  override fun addViewControllers(registry: ViewControllerRegistry) {
-    registry.addViewController("/notFound")
-      .setStatusCode(HttpStatus.OK)
-      .setViewName("forward:/")
+    registry
+      .addResourceHandler(
+        "/css/**",
+        "/fonts/**",
+        "/img/**",
+        "/js/**"
+      )
+      .addResourceLocations(
+        "classpath:public/css/",
+        "classpath:public/fonts/",
+        "classpath:public/img/",
+        "classpath:public/js/"
+      )
+      .setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS).cachePublic())
   }
 }
 
 @Component
-class CustomContainer : WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
-  override fun customize(factory: ConfigurableServletWebServerFactory) {
-    factory.addErrorPages(ErrorPage(HttpStatus.NOT_FOUND, "/notFound"))
+@ControllerAdvice
+class Customizer {
+  @ExceptionHandler(NoHandlerFoundException::class)
+  fun notFound(): String {
+    return "forward:/"
   }
 }

komga/src/main/resources/application.yml
@@ -7,10 +7,7 @@ spring:
     console:
       enabled: true
   resources:
-    cache:
-      cachecontrol:
-        cache-public: true
-        max-age: 365d
+    add-mappings: false
   jpa.properties:
     javax:
       persistence.sharedCache.mode: ENABLE_SELECTIVE
@@ -24,6 +21,8 @@ spring:
         region.factory_class: org.hibernate.cache.jcache.JCacheRegionFactory
   thymeleaf:
     prefix: classpath:/public/
+  mvc:
+    throw-exception-if-no-handler-found: true
 server.servlet.session.timeout: 7d
 
 management: