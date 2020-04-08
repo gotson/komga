d1c8b23f447e5b3bd196159692d43794d28cc44b
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2020-04-08 11:38:24 +0800

    fix(swagger): ignore non-functional parameters

--- komga/src/main/kotlin/org/gotson/komga/infrastructure/swagger/SwaggerConfiguration.kt
+++ komga/src/main/kotlin/org/gotson/komga/infrastructure/swagger/SwaggerConfiguration.kt
@@ -3,6 +3,8 @@ package org.gotson.komga.infrastructure.swagger
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.RequestHeader
+import org.springframework.web.context.request.WebRequest
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
@@ -12,8 +14,11 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2
 class SwaggerConfiguration {
 
   @Bean
-  fun getDocket(): Docket {
-    return Docket(DocumentationType.SWAGGER_2)
-        .ignoredParameterTypes(AuthenticationPrincipal::class.java)
-  }
+  fun getDocket(): Docket =
+    Docket(DocumentationType.SWAGGER_2)
+      .ignoredParameterTypes(
+        AuthenticationPrincipal::class.java,
+        WebRequest::class.java,
+        RequestHeader::class.java
+      )
 }
