1e9407cfe25a6104150efe2b7892a9973878ef0a
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2020-04-11 11:37:21 +0800

    fix(swagger): paging and sorting parameters showing incorrectly

--- komga/src/main/kotlin/org/gotson/komga/infrastructure/swagger/SwaggerConfiguration.kt
+++ komga/src/main/kotlin/org/gotson/komga/infrastructure/swagger/SwaggerConfiguration.kt
@@ -2,8 +2,11 @@ package org.gotson.komga.infrastructure.swagger
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.Ordered
+import org.springframework.data.domain.Pageable
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.context.request.WebRequest
+import springfox.documentation.schema.AlternateTypeRules
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
@@ -19,4 +22,15 @@ class SwaggerConfiguration {
         AuthenticationPrincipal::class.java,
         WebRequest::class.java
       )
+      .alternateTypeRules(AlternateTypeRules.newRule(
+        Pageable::class.java,
+        PageableMixin::class.java,
+        Ordered.HIGHEST_PRECEDENCE
+      ))
+
+  private class PageableMixin {
+    val page = 0
+    val size = 20
+    val sort = ""
+  }
 }
