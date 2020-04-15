02ee0eb4087a5b41e851d1bbcb5b1b21bfd880d1
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2020-04-15 09:21:27 +0800

    build: update dependencies

--- komga/build.gradle.kts
+++ komga/build.gradle.kts
@@ -3,14 +3,14 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
   run {
-    val kotlinVersion = "1.3.70"
+    val kotlinVersion = "1.3.72"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.allopen") version kotlinVersion
     kotlin("plugin.spring") version kotlinVersion
     kotlin("plugin.jpa") version kotlinVersion
     kotlin("kapt") version kotlinVersion
   }
-  id("org.springframework.boot") version "2.2.5.RELEASE"
+  id("org.springframework.boot") version "2.2.6.RELEASE"
   id("io.spring.dependency-management") version "1.0.9.RELEASE"
   id("com.github.ben-manes.versions") version "0.28.0"
   id("com.gorylenko.gradle-git-properties") version "2.2.2"
@@ -47,7 +47,7 @@ dependencies {
   implementation("com.github.ben-manes.caffeine:caffeine")
   implementation("com.github.ben-manes.caffeine:jcache")
 
-  implementation("io.github.microutils:kotlin-logging:1.7.8")
+  implementation("io.github.microutils:kotlin-logging:1.7.9")
   implementation("io.micrometer:micrometer-registry-influx")
 
   run {
@@ -64,9 +64,9 @@ dependencies {
   implementation("com.github.klinq:klinq-jpaspec:0.9")
 
   implementation("commons-io:commons-io:2.6")
-  implementation("org.apache.commons:commons-lang3:3.9")
+  implementation("org.apache.commons:commons-lang3:3.10")
 
-  implementation("org.apache.tika:tika-core:1.23")
+  implementation("org.apache.tika:tika-core:1.24")
   implementation("org.apache.commons:commons-compress:1.20")
   implementation("com.github.junrar:junrar:4.0.0")
   implementation("org.apache.pdfbox:pdfbox:2.0.19")
@@ -89,7 +89,7 @@ dependencies {
     exclude(module = "mockito-core")
   }
   testImplementation("org.springframework.security:spring-security-test")
-  testImplementation("com.ninja-squad:springmockk:2.0.0")
+  testImplementation("com.ninja-squad:springmockk:2.0.1")
   testImplementation("io.mockk:mockk:1.9.3")
   testImplementation("com.google.jimfs:jimfs:1.1")
 
