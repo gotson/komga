3e081f28854d4b480fe2e3fb1e5cd46c6f11eb94
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2020-04-24 15:53:16 +0800

    test: change JMS queue emptying afterEach

--- komga/src/test/kotlin/org/gotson/komga/infrastructure/jms/ArtemisConfigTest.kt
+++ komga/src/test/kotlin/org/gotson/komga/infrastructure/jms/ArtemisConfigTest.kt
@@ -2,7 +2,7 @@ package org.gotson.komga.infrastructure.jms
 
 import mu.KotlinLogging
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
@@ -25,7 +25,7 @@ class ArtemisConfigTest(
     jmsTemplate.receiveTimeout = JmsDestinationAccessor.RECEIVE_TIMEOUT_NO_WAIT
   }
 
-  @BeforeEach
+  @AfterEach
   fun emptyQueue() {
     while (jmsTemplate.receive(QUEUE_TASKS) != null) {
       logger.info { "Emptying queue" }
