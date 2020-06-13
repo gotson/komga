bf571519d7fafe9e7b442aae218bb8b7e7f2cd7b
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2020-06-13 21:14:42 +0800

    ci: generate openapi spec and commit back to repo on release

--- .travis/prepare-release.sh
+++ .travis/prepare-release.sh
@@ -6,6 +6,7 @@ echo version=$1 >gradle.properties
 # Build jar
 ./gradlew copyWebDist
 ./gradlew assemble
+./gradlew generateOpenApiDocs
 
 # Prepare Dockerhub release
 source "$(dirname "$0")/prepare-dockerhub.sh" $1
