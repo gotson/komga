3105fc4ef4a7e24da7063acd3dc0d951cd865485
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2020-07-16 07:47:25 +0800

    ci: deploy pre-releases

--- .travis/docker-common.sh
+++ .travis/docker-common.sh
@@ -1,2 +1,11 @@
+# Arguments
+# 1: channel
+
 export DOCKER_CLI_EXPERIMENTAL=enabled
 PLATFORMS=linux/amd64,linux/arm/v7,linux/arm64/v8,linux/ppc64le,linux/s390x
+
+if [ -z "$1" ]; then
+    DOCKER_CHANNEL="latest"
+else
+    DOCKER_CHANNEL=$1
+fi
