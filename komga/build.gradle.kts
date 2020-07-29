33c179a4838f0fd333cce5290f3437a07a6f7f02
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2020-07-29 21:39:46 +0800

    build: update dependencies

--- komga/build.gradle.kts
+++ komga/build.gradle.kts
@@ -17,9 +17,9 @@ plugins {
     kotlin("kapt") version kotlinVersion
   }
   id("org.springframework.boot") version "2.3.2.RELEASE"
-  id("com.github.ben-manes.versions") version "0.28.0"
-  id("com.gorylenko.gradle-git-properties") version "2.2.2"
-  id("com.rohanprabhu.kotlin-dsl-jooq") version "0.4.5"
+  id("com.github.ben-manes.versions") version "0.29.0"
+  id("com.gorylenko.gradle-git-properties") version "2.2.3"
+  id("com.rohanprabhu.kotlin-dsl-jooq") version "0.4.6"
   id("org.flywaydb.flyway") version "6.4.4"
   id("com.github.johnrengelman.processes") version "0.5.0"
   id("org.springdoc.openapi-gradle-plugin") version "1.3.0"
@@ -54,12 +54,12 @@ dependencies {
 
   implementation("org.flywaydb:flyway-core")
 
-  implementation("io.github.microutils:kotlin-logging:1.7.9")
+  implementation("io.github.microutils:kotlin-logging:1.8.3")
   implementation("io.micrometer:micrometer-registry-influx")
   implementation("io.hawt:hawtio-springboot:2.10.1")
 
   run {
-    val springdocVersion = "1.3.4"
+    val springdocVersion = "1.4.3"
     implementation("org.springdoc:springdoc-openapi-ui:$springdocVersion")
     implementation("org.springdoc:springdoc-openapi-security:$springdocVersion")
   }
@@ -67,19 +67,19 @@ dependencies {
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
   implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml")
 
-  implementation("commons-io:commons-io:2.6")
-  implementation("org.apache.commons:commons-lang3:3.10")
+  implementation("commons-io:commons-io:2.7")
+  implementation("org.apache.commons:commons-lang3:3.11")
 
   implementation("org.apache.tika:tika-core:1.24.1")
   implementation("org.apache.commons:commons-compress:1.20")
   implementation("com.github.junrar:junrar:7.2.0")
-  implementation("org.apache.pdfbox:pdfbox:2.0.19")
+  implementation("org.apache.pdfbox:pdfbox:2.0.20")
   implementation("net.grey-panther:natural-comparator:1.1")
   implementation("org.jsoup:jsoup:1.13.1")
 
   implementation("net.coobird:thumbnailator:0.4.11")
-  runtimeOnly("com.twelvemonkeys.imageio:imageio-jpeg:3.5")
-  runtimeOnly("com.twelvemonkeys.imageio:imageio-tiff:3.5")
+  runtimeOnly("com.twelvemonkeys.imageio:imageio-jpeg:3.6")
+  runtimeOnly("com.twelvemonkeys.imageio:imageio-tiff:3.6")
   runtimeOnly(files("$projectDir/libs/webp-imageio-decoder-plugin-0.2.jar"))
   // support for jpeg2000
   runtimeOnly("com.github.jai-imageio:jai-imageio-jpeg2000:1.3.0")
@@ -90,18 +90,18 @@ dependencies {
   implementation("com.github.f4b6a3:tsid-creator:2.2.4")
 
   runtimeOnly("com.h2database:h2:1.4.200")
-  runtimeOnly("org.xerial:sqlite-jdbc:3.32.3")
-  jooqGeneratorRuntime("org.xerial:sqlite-jdbc:3.32.3")
+  runtimeOnly("org.xerial:sqlite-jdbc:3.32.3.2")
+  jooqGeneratorRuntime("org.xerial:sqlite-jdbc:3.32.3.2")
 
   testImplementation("org.springframework.boot:spring-boot-starter-test") {
     exclude(module = "mockito-core")
   }
   testImplementation("org.springframework.security:spring-security-test")
-  testImplementation("com.ninja-squad:springmockk:2.0.1")
+  testImplementation("com.ninja-squad:springmockk:2.0.2")
   testImplementation("io.mockk:mockk:1.10.0")
   testImplementation("com.google.jimfs:jimfs:1.1")
 
-  testImplementation("com.tngtech.archunit:archunit-junit5:0.13.1")
+  testImplementation("com.tngtech.archunit:archunit-junit5:0.14.1")
 
   developmentOnly("org.springframework.boot:spring-boot-devtools:2.3.2.RELEASE")
 }
