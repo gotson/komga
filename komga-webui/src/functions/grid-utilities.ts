18280beb95b44bb9dfeec9f81cfe04b97c225fdd
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2020-08-14 16:49:20 +0800

    fix(webui): change grid size for sm screens

--- komga-webui/src/functions/grid-utilities.ts
+++ komga-webui/src/functions/grid-utilities.ts
@@ -2,8 +2,6 @@ export function computeCardWidth (width: number, breakpoint: string, cardPadding
   switch (breakpoint) {
     case 'xs':
       return (width - (cardPadding * 2)) / 2
-    case 'sm':
-      return (width - (cardPadding * 3)) / 3
     default:
       return 150
   }
