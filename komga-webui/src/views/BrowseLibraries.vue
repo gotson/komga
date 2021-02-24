d9b28ca91a37737f5abe720bd4057d6432249845
Ben Kuskopf <kussie@gmail.com>
2021-02-24 15:39:11 +1000

    Make series tag and genre pills clickable. Adds a watch command to the webui to hot reload on changes.

--- komga-webui/src/views/BrowseLibraries.vue
+++ komga-webui/src/views/BrowseLibraries.vue
@@ -286,7 +286,8 @@ export default Vue.extend({
         this.$cookies.get(this.cookieSort(route.params.libraryId)) ||
         this.$_.clone(this.sortDefault)
 
-      // This is a bit of a hack as filterOptions wouldn't otherwise be populated at this stage
+      // This is a bit of a hack as filterOptions wouldn't otherwise be populated at this stage which
+      // causes the parseQueryFilter calls in the below if statement to always return false for anything referencing filterOptions
       const requestLibraryId = this.$route.params.libraryId !== LIBRARIES_ALL ? this.$route.params.libraryId : undefined
       this.filterOptions.genre = toNameValue(await this.$komgaReferential.getGenres(requestLibraryId))
       this.filterOptions.tag = toNameValue(await this.$komgaReferential.getTags(requestLibraryId))
