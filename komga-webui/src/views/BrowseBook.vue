dc0cc1380793bf870e11444debf1872a6cefc95c
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2021-04-26 16:48:57 +0800

    feat(webui): ability to read incognito

--- komga-webui/src/views/BrowseBook.vue
+++ komga-webui/src/views/BrowseBook.vue
@@ -122,6 +122,18 @@
                   {{ $t('common.read') }}
                 </v-btn>
               </v-col>
+
+              <v-col cols="auto">
+                <v-btn small
+                       :title="$t('browse_book.read_book')"
+                       :to="{name: 'read-book', params: { bookId: bookId}, query: { context: context.origin, contextId: context.id, incognito: true}}"
+                       :disabled="book.media.status !== 'READY' || !canReadPages"
+                >
+                  <v-icon left small>mdi-incognito</v-icon>
+                  {{ $t('common.read') }}
+                </v-btn>
+              </v-col>
+
               <v-col cols="auto">
                 <v-btn :title="$t('browse_book.download_file')"
                        small
@@ -155,6 +167,18 @@
               {{ $t('common.read') }}
             </v-btn>
           </v-col>
+
+          <v-col cols="auto">
+            <v-btn small
+                   :title="$t('browse_book.read_book')"
+                   :to="{name: 'read-book', params: { bookId: bookId}, query: { context: context.origin, contextId: context.id, incognito: true}}"
+                   :disabled="book.media.status !== 'READY' || !canReadPages"
+            >
+              <v-icon left small>mdi-incognito</v-icon>
+              {{ $t('common.read') }}
+            </v-btn>
+          </v-col>
+
           <v-col cols="auto">
             <v-btn :title="$t('browse_book.download_file')"
                    small
