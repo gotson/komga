f4523c82999dbe1ef00e29555d73f005ad126e5e
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2021-08-30 16:57:20 +0800

    ci: drop linux/ppc64le and linux/s390x for docker images

--- ci/docker-common.sh
+++ ci/docker-common.sh
@@ -3,7 +3,7 @@
 # 2: channel
 
 export DOCKER_CLI_EXPERIMENTAL=enabled
-PLATFORMS=linux/amd64,linux/arm/v7,linux/arm64/v8,linux/ppc64le,linux/s390x
+PLATFORMS=linux/amd64,linux/arm/v7,linux/arm64/v8
 
 if [ -z "$2" ]; then
     DOCKER_CHANNEL="latest"
