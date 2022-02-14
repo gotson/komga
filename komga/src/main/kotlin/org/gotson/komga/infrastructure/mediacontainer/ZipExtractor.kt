57082bd990dfc450f23014f57ccbb862e40dacfb
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2022-02-14 10:45:51 +0800

    fix: remove caching of ZipFile
    closes #801

--- komga/src/main/kotlin/org/gotson/komga/infrastructure/mediacontainer/ZipExtractor.kt
+++ komga/src/main/kotlin/org/gotson/komga/infrastructure/mediacontainer/ZipExtractor.kt
@@ -1,6 +1,5 @@
 package org.gotson.komga.infrastructure.mediacontainer
 
-import com.github.benmanes.caffeine.cache.Caffeine
 import mu.KotlinLogging
 import net.greypanther.natsort.CaseInsensitiveSimpleNaturalComparator
 import org.apache.commons.compress.archivers.ArchiveEntry
@@ -10,7 +9,6 @@ import org.gotson.komga.domain.model.MediaType
 import org.gotson.komga.infrastructure.image.ImageAnalyzer
 import org.springframework.stereotype.Service
 import java.nio.file.Path
-import java.util.concurrent.TimeUnit
 
 private val logger = KotlinLogging.logger {}
 
@@ -19,12 +17,6 @@ class ZipExtractor(
   private val contentDetector: ContentDetector,
   private val imageAnalyzer: ImageAnalyzer,
 ) : MediaContainerExtractor {
-
-  private val cache = Caffeine.newBuilder()
-    .maximumSize(20)
-    .expireAfterAccess(1, TimeUnit.MINUTES)
-    .evictionListener { _: Path?, zip: ZipFile?, _ -> zip?.close() }
-    .build<Path, ZipFile>()
 
   private val natSortComparator: Comparator<String> = CaseInsensitiveSimpleNaturalComparator.getInstance()
 
@@ -53,8 +45,8 @@ class ZipExtractor(
         .sortedWith(compareBy(natSortComparator) { it.name })
     }
 
-  override fun getEntryStream(path: Path, entryName: String): ByteArray {
-    val zip = cache.get(path) { ZipFile(path.toFile()) }!!
-    return zip.getInputStream(zip.getEntry(entryName)).use { it.readBytes() }
-  }
+  override fun getEntryStream(path: Path, entryName: String): ByteArray =
+    ZipFile(path.toFile()).use { zip ->
+      zip.getInputStream(zip.getEntry(entryName)).use { it.readBytes() }
+    }
 }
