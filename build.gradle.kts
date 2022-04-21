edff88275d25800e2bd761a98caf9d0a9e89aa6d
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2022-04-21 16:21:08 +0800

    build: update Kotlin to 1.6.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 
 plugins {
   run {
-    val kotlinVersion = "1.6.10"
+    val kotlinVersion = "1.6.21"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.spring") version kotlinVersion
     kotlin("kapt") version kotlinVersion
