50524b9e5d50b7ff08d3213fcd84dd791c943989
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2022-11-10 17:07:02 +0800

    refactor: replace artemis deprecated usage

--- komga/src/main/kotlin/org/gotson/komga/infrastructure/jms/ArtemisConfig.kt
+++ komga/src/main/kotlin/org/gotson/komga/infrastructure/jms/ArtemisConfig.kt
@@ -31,7 +31,7 @@ class ArtemisConfig : ArtemisConfigurationCustomizer {
       // default is 90, meaning the queue would block if disk is 90% full. Set it to 100 to avoid blocking.
       it.maxDiskUsage = 100
       // disable prefetch, ensures messages stay in the queue and last value can have desired effect
-      it.addAddressesSetting(
+      it.addAddressSetting(
         QUEUE_TASKS,
         AddressSettings().apply {
           defaultConsumerWindowSize = 0
