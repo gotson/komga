37514f0c21f06f97985b276a39075a5f20ff2546
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2023-05-23 17:37:39 +0800

    build(deps): bump pdfbox to 2.0.28

--- komga/build.gradle.kts
+++ komga/build.gradle.kts
@@ -87,7 +87,7 @@ dependencies {
   implementation("org.apache.tika:tika-core:2.7.0")
   implementation("org.apache.commons:commons-compress:1.23.0")
   implementation("com.github.junrar:junrar:7.5.4")
-  implementation("org.apache.pdfbox:pdfbox:2.0.27")
+  implementation("org.apache.pdfbox:pdfbox:2.0.28")
   implementation("net.grey-panther:natural-comparator:1.1")
   implementation("org.jsoup:jsoup:1.16.1")
 
