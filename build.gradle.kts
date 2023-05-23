5e8943121fc1df0ab535bc73a273028d3eaa93d9
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2023-05-23 17:31:26 +0800

    build(deps): bump ben-manes.versions to 0.46.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
     kotlin("kapt") version kotlinVersion
   }
   id("org.jlleitschuh.gradle.ktlint") version "11.1.0"
-  id("com.github.ben-manes.versions") version "0.45.0"
+  id("com.github.ben-manes.versions") version "0.46.0"
 }
 
 fun isNonStable(version: String): Boolean {
