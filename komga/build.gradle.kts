9c977d3b45429a953cdd2e006226aeb833977e10
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2023-05-23 17:28:23 +0800

    build(deps): bump joog gradle plugin to 8.2

--- komga/build.gradle.kts
+++ komga/build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
   }
   id("org.springframework.boot") version "3.1.0"
   id("com.gorylenko.gradle-git-properties") version "2.4.1"
-  id("nu.studer.jooq") version "8.1"
+  id("nu.studer.jooq") version "8.2"
   id("org.flywaydb.flyway") version "9.7.0"
   id("com.github.johnrengelman.processes") version "0.5.0"
   id("org.springdoc.openapi-gradle-plugin") version "1.6.0"
