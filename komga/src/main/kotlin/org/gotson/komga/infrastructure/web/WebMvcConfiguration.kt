7315df54d6918617d04e4d92e05048b85c63f258
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2023-09-11 17:14:25 +0800

    fix(api): unknown API endpoints now return 404 instead of redirecting to index

--- komga/src/main/kotlin/org/gotson/komga/infrastructure/web/WebMvcConfiguration.kt
+++ komga/src/main/kotlin/org/gotson/komga/infrastructure/web/WebMvcConfiguration.kt
@@ -1,11 +1,14 @@
 package org.gotson.komga.infrastructure.web
 
+import jakarta.servlet.http.HttpServletRequest
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.CacheControl
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.server.ResponseStatusException
 import org.springframework.web.servlet.NoHandlerFoundException
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
@@ -90,9 +93,12 @@ class WebMvcConfiguration : WebMvcConfigurer {
 
 @Component
 @ControllerAdvice
-class Customizer {
+class ResourceNotFoundController {
+  val apis = listOf("/api", "/opds", "/sse")
+
   @ExceptionHandler(NoHandlerFoundException::class)
-  fun notFound(): String {
+  fun notFound(request: HttpServletRequest): String {
+    if (apis.any { request.requestURI.startsWith(it, true) }) throw ResponseStatusException(HttpStatus.NOT_FOUND)
     return "forward:/"
   }
 }
