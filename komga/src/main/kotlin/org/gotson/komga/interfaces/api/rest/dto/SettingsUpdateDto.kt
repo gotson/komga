62709861bcfd7d9c275e5aaa9de793c742802801
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2023-09-25 15:51:49 +0800

    fix(api): missing validation

--- komga/src/main/kotlin/org/gotson/komga/interfaces/api/rest/dto/SettingsUpdateDto.kt
+++ komga/src/main/kotlin/org/gotson/komga/interfaces/api/rest/dto/SettingsUpdateDto.kt
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.Positive
 data class SettingsUpdateDto(
   val deleteEmptyCollections: Boolean? = null,
   val deleteEmptyReadLists: Boolean? = null,
-  @Positive
+  @get:Positive
   val rememberMeDurationDays: Long? = null,
   val renewRememberMeKey: Boolean? = null,
 )
