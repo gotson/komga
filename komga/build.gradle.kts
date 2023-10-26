07cf63a7b7181fa3a96a2033267b89c769438787
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2023-10-26 15:43:22 +0800

    deps(komga): add imageio-heif:0.6.0
    Closes: #942

--- komga/build.gradle.kts
+++ komga/build.gradle.kts
@@ -98,7 +98,8 @@ dependencies {
   runtimeOnly("com.twelvemonkeys.imageio:imageio-jpeg:3.9.4")
   runtimeOnly("com.twelvemonkeys.imageio:imageio-tiff:3.9.4")
   runtimeOnly("com.twelvemonkeys.imageio:imageio-webp:3.9.4")
-  runtimeOnly("com.github.gotson.nightmonkeys:imageio-jxl:0.4.1")
+  runtimeOnly("com.github.gotson.nightmonkeys:imageio-jxl:0.6.0")
+  runtimeOnly("com.github.gotson.nightmonkeys:imageio-heif:0.6.0")
   // support for jpeg2000
   runtimeOnly("com.github.jai-imageio:jai-imageio-jpeg2000:1.4.0")
   runtimeOnly("org.apache.pdfbox:jbig2-imageio:3.0.4")
