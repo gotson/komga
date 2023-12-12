834b51d744747eed6d117a7b55232ec63a11106f
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2023-12-12 15:14:54 +0800

    feat(opds-v1): use static xml namespace prefix for opds-pse
    this should increase compatibility with poorly implemented OPDS clients

--- komga/src/main/kotlin/org/gotson/komga/interfaces/api/opds/v1/dto/XmlNamespaces.kt
+++ komga/src/main/kotlin/org/gotson/komga/interfaces/api/opds/v1/dto/XmlNamespaces.kt
@@ -3,3 +3,7 @@ package org.gotson.komga.interfaces.api.opds.v1.dto
 const val ATOM = "http://www.w3.org/2005/Atom"
 const val OPDS_PSE = "http://vaemendis.net/opds-pse/ns"
 const val OPENSEARCH = "http://a9.com/-/spec/opensearch/1.1/"
+
+val prefixToNamespace = mapOf(
+  "pse" to OPDS_PSE,
+)
