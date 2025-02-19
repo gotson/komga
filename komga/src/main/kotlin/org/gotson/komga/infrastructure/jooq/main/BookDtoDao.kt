9bab9f620fb480f365c8df20238b6ab711ae73a9
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2025-02-19 15:46:33 +0800

    feat(api): books can be sorted by series title

--- komga/src/main/kotlin/org/gotson/komga/infrastructure/jooq/main/BookDtoDao.kt
+++ komga/src/main/kotlin/org/gotson/komga/infrastructure/jooq/main/BookDtoDao.kt
@@ -72,6 +72,7 @@ class BookDtoDao(
   private val sorts =
     mapOf(
       "name" to b.NAME.collate(SqliteUdfDataSource.COLLATION_UNICODE_3),
+      "series" to sd.TITLE_SORT.noCase(),
       "created" to b.CREATED_DATE,
       "createdDate" to b.CREATED_DATE,
       "lastModified" to b.LAST_MODIFIED_DATE,
