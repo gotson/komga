fd8b05fbe7d305ab10f88c319a2bf373b6e33eb6
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2025-02-24 12:28:17 +0800

    docs(api): update cookie names

--- komga/src/main/kotlin/org/gotson/komga/infrastructure/openapi/OpenApiConfiguration.kt
+++ komga/src/main/kotlin/org/gotson/komga/infrastructure/openapi/OpenApiConfiguration.kt
@@ -98,14 +98,14 @@ class OpenApiConfiguration(
 
             Upon successful authentication, a session is created, and can be reused.
 
-            - By default, a `SESSION` cookie is set via `Set-Cookie` response header. This works well for browsers and clients that can handle cookies.
+            - By default, a `KOMGA-SESSION` cookie is set via `Set-Cookie` response header. This works well for browsers and clients that can handle cookies.
             - If you specify a header `X-Auth-Token` during authentication, the session ID will be returned via this same header. You can then pass that header again for subsequent requests to reuse the session.
 
             If you need to set the session cookie later on, you can call `/api/v1/login/set-cookie` with `X-Auth-Token`. The response will contain the `Set-Cookie` header.
 
             ## Remember Me
 
-            During authentication, if a request parameter `remember-me` is passed and set to `true`, the server will also return a `remember-me` cookie. This cookie will be used to login automatically even if the session has expired.
+            During authentication, if a request parameter `remember-me` is passed and set to `true`, the server will also return a `komga-remember-me` cookie. This cookie will be used to login automatically even if the session has expired.
 
             ## Logout
 
