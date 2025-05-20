2dba671bd99cf6ed14e0835d4552574854373609
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2025-05-20 15:16:47 +0800

    announcements

---
+++ next-ui/src/colada/mutations/mark-announcements-read.ts
@@ -0,0 +1,16 @@
+import {defineMutation, useMutation, useQueryCache} from '@pinia/colada'
+import {komgaClient} from '@/api/komga-client'
+
+export const useMarkAnnouncementsRead = defineMutation(() => {
+  const queryCache = useQueryCache()
+  return useMutation({
+    mutation: (announcementIds: string[]) =>
+      komgaClient.PUT('/api/v1/announcements', {body: announcementIds}),
+    onSuccess: () => {
+      queryCache.invalidateQueries({key: ['announcements']})
+    },
+    onError: (error) => {
+      console.log('announcements mark read error', error)
+    },
+  })
+})
