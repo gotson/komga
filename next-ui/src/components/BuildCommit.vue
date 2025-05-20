9c376d0d6892f28e720c9e7985cbe7f1b4d7c2ac
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2025-05-20 10:55:39 +0800

    next-ui wip

---
+++ next-ui/src/components/BuildCommit.vue
@@ -0,0 +1,23 @@
+<template>
+  <template v-if="commit">
+    <v-btn
+      prepend-icon="mdi-source-commit"
+      variant="text"
+      color="grey"
+      size="small"
+      class="text-caption"
+      :href="'https://github.com/gotson/komga/commits/' + commit"
+      target="_blank"
+    >
+      {{ commit }}
+    </v-btn>
+  </template>
+</template>
+
+<script setup lang="ts">
+import {useActuatorInfo} from '@/colada/queries/actuator-info'
+
+const {data} = useActuatorInfo()
+
+const commit = computed(() => data.value?.git?.commit?.id)
+</script>
