bebd61bd1ee9aa42655d0a430ed1b599224d50b4
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2025-05-20 10:55:39 +0800

    next-ui wip

---
+++ next-ui/src/components/app/drawer/AppDrawerMenuLogout.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-list-item
+    title="Logout"
+    prepend-icon="mdi-power"
+    @click="performLogout"
+  />
+</template>
+
+<script setup lang="ts">
+import {useLogout} from '@/colada/mutations/logout'
+
+const router = useRouter()
+const {mutateAsync: logoutAsync} = useLogout()
+
+async function performLogout() {
+  logoutAsync().then(() => router.push('/login'))
+}
+</script>
