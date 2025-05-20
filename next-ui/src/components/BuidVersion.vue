e0d8311bf85353ca4e218eb896b8d153451acb65
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2025-05-20 10:55:39 +0800

    next-ui wip

---
+++ next-ui/src/components/BuidVersion.vue
@@ -0,0 +1,20 @@
+<template>
+  <template v-if="buildVersion">
+    <v-btn
+      prepend-icon="mdi-tag-outline"
+      variant="text"
+      color="grey"
+      size="small"
+      class="text-caption"
+      to="/server/updates"
+    >
+      {{ buildVersion }}
+    </v-btn>
+  </template>
+</template>
+
+<script setup lang="ts">
+import {useBuildVersion} from '@/composables/buid-version.ts'
+
+const {buildVersion} = useBuildVersion()
+</script>
