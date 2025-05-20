9e2eb6abff0e01c8b0d8a430ab3b8cd21b3c5232
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2025-05-20 11:07:08 +0800

    display badge on version number if updates are available

--- next-ui/src/components/BuidVersion.vue
+++ next-ui/src/components/BuidVersion.vue
@@ -1,20 +1,28 @@
 <template>
   <template v-if="buildVersion">
-    <v-btn
-      prepend-icon="mdi-tag-outline"
-      variant="text"
-      color="grey"
-      size="small"
-      class="text-caption"
-      to="/server/updates"
+    <v-badge
+      dot
+      color="warning"
+      :model-value="isLatestVersion == false"
     >
-      {{ buildVersion }}
-    </v-btn>
+      <v-btn
+        prepend-icon="mdi-tag-outline"
+        variant="text"
+        color="grey"
+        size="small"
+        class="text-caption"
+        to="/server/updates"
+      >
+        {{ buildVersion }}
+      </v-btn>
+    </v-badge>
   </template>
 </template>
 
 <script setup lang="ts">
 import {useBuildVersion} from '@/composables/buid-version.ts'
+import {useLatestVersion} from '@/composables/latest-version.ts'
 
 const {buildVersion} = useBuildVersion()
+const {isLatestVersion} = useLatestVersion()
 </script>
