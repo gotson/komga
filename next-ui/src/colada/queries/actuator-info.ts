4c668b8689695429ce054b08cd353aefc93daa13
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2025-05-21 10:19:51 +0800

    refactor colada composables

--- next-ui/src/colada/queries/actuator-info.ts
+++ next-ui/src/colada/queries/actuator-info.ts
@@ -3,7 +3,7 @@ import {komgaClient} from '@/api/komga-client'
 import type {ActuatorInfo} from '@/types/Actuator'
 
 export const useActuatorInfo = defineQuery(() => {
-  return useQuery({
+  const {data, ...rest} = useQuery({
     key: () => ['actuator-info'],
     query: () => komgaClient.GET('/actuator/info')
       // unwrap the openapi-fetch structure on success
@@ -12,4 +12,14 @@ export const useActuatorInfo = defineQuery(() => {
     staleTime: 60 * 60 * 1000,
     gcTime: false,
   })
+
+  const buildVersion = computed(() => data.value?.build?.version)
+  const commitId = computed(() => data.value?.git?.commit?.id)
+
+  return {
+    data,
+    ...rest,
+    buildVersion,
+    commitId,
+  }
 })
