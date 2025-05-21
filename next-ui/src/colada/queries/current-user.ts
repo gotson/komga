36a4cc27abd21a5062b2d8f4242baf424fb8ca22
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2025-05-21 10:19:51 +0800

    refactor colada composables

--- next-ui/src/colada/queries/current-user.ts
+++ next-ui/src/colada/queries/current-user.ts
@@ -1,8 +1,9 @@
 import {defineQuery, useQuery} from '@pinia/colada'
 import {komgaClient} from '@/api/komga-client'
+import {UserRoles} from '@/types/UserRoles.ts'
 
 export const useCurrentUser = defineQuery(() => {
-  return useQuery({
+  const {data, ...rest} = useQuery({
     key: () => ['current-user'],
     query: () => komgaClient.GET('/api/v2/users/me')
       // unwrap the openapi-fetch structure on success
@@ -12,4 +13,14 @@ export const useCurrentUser = defineQuery(() => {
     gcTime: false,
     autoRefetch: true,
   })
+
+  const hasRole =(role: UserRoles) => data.value?.roles.includes(role)
+  const isAdmin = computed(() => hasRole(UserRoles.ADMIN))
+
+  return {
+    data,
+    ...rest,
+    hasRole,
+    isAdmin,
+  }
 })
