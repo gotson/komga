66e397c054623d01541a5be676a0c0d122387161
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2025-05-21 15:00:50 +0800

    first shot at users view

---
+++ next-ui/src/colada/queries/users.ts
@@ -0,0 +1,11 @@
+import {defineQuery, useQuery} from '@pinia/colada'
+import {komgaClient} from '@/api/komga-client'
+
+export const useUsers = defineQuery(() => {
+  return useQuery({
+    key: () => ['users'],
+    query: () => komgaClient.GET('/api/v2/users')
+      // unwrap the openapi-fetch structure on success
+      .then((res) => res.data),
+  })
+})
