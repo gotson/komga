d5f6a009f7a18e5ae5add32e95c50feee503efa1
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2025-05-21 10:19:51 +0800

    refactor colada composables

--- next-ui/src/composables/buid-version.ts
+++
@@ -1,9 +0,0 @@
-  import {useActuatorInfo} from '@/colada/queries/actuator-info'
-
-export function useBuildVersion() {
-  const {data} = useActuatorInfo()
-
-  const buildVersion = computed(() => data.value?.build?.version)
-
-  return {buildVersion}
-}
