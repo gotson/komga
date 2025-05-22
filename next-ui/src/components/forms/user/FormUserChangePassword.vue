f1b3f378251ad35b5c25421b06a6063b0860530e
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2025-05-22 16:35:31 +0800

    users dialog wip

---
+++ next-ui/src/components/forms/user/FormUserChangePassword.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-text-field />
+  <v-text-field />
+</template>
+
+<script setup lang="ts">
+import type {components} from '@/generated/openapi/komga'
+
+const user = defineModel<components["schemas"]["UserDto"] | undefined>({required: true})
+</script>
