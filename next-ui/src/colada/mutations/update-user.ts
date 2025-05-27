ce0dc37ae67784d93b0da2842ac63b05632dbeab
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2025-05-27 15:17:47 +0800

    user creation and revamp edit dialog to support restrictions

--- next-ui/src/colada/mutations/update-user.ts
+++ next-ui/src/colada/mutations/update-user.ts
@@ -2,6 +2,21 @@ import {defineMutation, useMutation, useQueryCache} from '@pinia/colada'
 import {komgaClient} from '@/api/komga-client'
 import type {components} from '@/generated/openapi/komga'
 
+export const useCreateUser = defineMutation(() => {
+  const queryCache = useQueryCache()
+  return useMutation({
+    mutation: (user: components['schemas']['UserCreationDto']) =>
+      komgaClient.POST('/api/v2/users', {
+        body: user,
+      }),
+    onSuccess: () => {
+      void queryCache.invalidateQueries({key: ['users']})
+    },
+    onError: (error) => {
+      console.log('create user error', error)
+    },
+  })
+})
 export const useUpdateUser = defineMutation(() => {
   const queryCache = useQueryCache()
   return useMutation({
