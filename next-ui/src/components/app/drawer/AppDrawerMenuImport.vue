7b0d5f4f75161f24c0f699dce164a364d75aca0a
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2025-05-28 16:32:45 +0800

    i18n support

--- next-ui/src/components/app/drawer/AppDrawerMenuImport.vue
+++ next-ui/src/components/app/drawer/AppDrawerMenuImport.vue
@@ -3,19 +3,31 @@
     <template #activator="{ props }">
       <v-list-item
         v-bind="props"
-        title="Import"
+        :title="$formatMessage({
+          description: 'Drawer menu for Import',
+          defaultMessage: 'Import',
+          id: 'N7+QXi'
+        })"
         prepend-icon="mdi-import"
       />
     </template>
 
     <v-list-item
       to="/import/books"
-      title="Books"
+      :title="$formatMessage({
+        description: 'Drawer menu for Import > Books',
+        defaultMessage: 'Books',
+        id: 'fQIepD'
+      })"
     />
 
     <v-list-item
       to="/import/readlist"
-      title="Read List"
+      :title="$formatMessage({
+        description: 'Drawer menu for Import > Read List',
+        defaultMessage: 'Read List',
+        id: 'Y6VlM9'
+      })"
     />
   </v-list-group>
 </template>
