6c97832601d181ab267e233e648abc5d5f38af9e
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2025-06-04 10:18:48 +0800

    quasar

---
+++ tsugini/src/components/app/drawer/menu/Logout.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-item
+    clickable
+    @click="performLogout"
+  >
+    <q-item-section avatar>
+      <q-icon name="mdi-power" />
+    </q-item-section>
+    <q-item-section>{{
+      $formatMessage({
+        description: 'Drawer menu for Logout',
+        defaultMessage: 'Logout',
+        id: 'ti4Pzo',
+      })
+    }}</q-item-section>
+  </q-item>
+</template>
+
+<script setup lang="ts">
+import { useLogout } from 'colada/mutations/logout'
+
+const router = useRouter()
+const { mutateAsync: logoutAsync } = useLogout()
+
+function performLogout() {
+  void logoutAsync().then(() => router.push('/login'))
+}
+</script>
