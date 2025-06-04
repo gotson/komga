d3d5a4a828ad8b3ed4d4c68cab0b57df35e70c2d
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2025-06-04 10:18:48 +0800

    quasar

---
+++ tsugini/src/pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <example-component
+      title="Example component"
+      active
+      :todos="todos"
+      :meta="meta"
+    ></example-component>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import type { Todo, Meta } from 'components/models'
+
+const todos = ref<Todo[]>(
+  [...Array(100).keys()].map((i) => ({
+    id: i + 1,
+    content: `ct${i + 1}`,
+  })),
+)
+
+const meta = ref<Meta>({
+  totalCount: 1200,
+})
+</script>
---
+++ tsugini/src/router/index.ts
@@ -0,0 +1,47 @@
+import { defineRouter } from '#q-app/wrappers'
+import {
+  createMemoryHistory,
+  createRouter,
+  createWebHashHistory,
+  createWebHistory,
+} from 'vue-router'
+import { routes, handleHotUpdate } from 'vue-router/auto-routes'
+import { setupLayouts } from 'virtual:generated-layouts'
+import { useLoginGuard } from 'src/router/login-guard'
+import { useRoleGuard } from 'src/router/role-guard'
+
+/*
+ * If not building with SSR mode, you can
+ * directly export the Router instantiation;
+ *
+ * The function below can be async too; either use
+ * async/await or return a Promise which resolves
+ * with the Router instance.
+ */
+
+export default defineRouter(function (/* { store, ssrContext } */) {
+  const createHistory = process.env.SERVER
+    ? createMemoryHistory
+    : process.env.VUE_ROUTER_MODE === 'history'
+      ? createWebHistory
+      : createWebHashHistory
+
+  const router = createRouter({
+    scrollBehavior: () => ({ left: 0, top: 0 }),
+    routes: setupLayouts(routes),
+
+    // Leave this as is and make changes in quasar.conf.js instead!
+    // quasar.conf.js -> build -> vueRouterMode
+    // quasar.conf.js -> build -> publicPath
+    history: createHistory(process.env.VUE_ROUTER_BASE),
+  })
+
+  if (import.meta.hot) {
+    handleHotUpdate(router)
+  }
+
+  useLoginGuard(router)
+  useRoleGuard(router)
+
+  return router
+})
