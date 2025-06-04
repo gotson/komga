aca01317ad4907fff79f9b88eb2d735390b87a01
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2025-06-04 10:18:48 +0800

    quasar

---
+++ tsugini/src/components/app/drawer/Footer.vue
@@ -0,0 +1,38 @@
+<!--<template>-->
+<!--  <q-separator />-->
+
+<!--  <div class="d-flex align-center text-caption text-medium-emphasis pa-2">-->
+<!--    <div class="d-flex ms-auto">-->
+<!--      <q-btn-->
+<!--        icon="mdi-help-circle-outline"-->
+<!--        href="https://komga.org"-->
+<!--        target="_blank"-->
+<!--        :text="-->
+<!--          $formatMessage({-->
+<!--            description: 'Drawer menu footer: documentation link',-->
+<!--            defaultMessage: 'Documentation',-->
+<!--            id: 'ccAMWS',-->
+<!--          })-->
+<!--        "-->
+<!--      />-->
+<!--    </div>-->
+<!--  </div>-->
+
+<!--  <div-->
+<!--    v-if="isAdmin"-->
+<!--    class="d-flex align-center text-caption text-medium-emphasis pa-2"-->
+<!--  >-->
+<!--    <div class="d-flex ms-auto">-->
+<!--      &lt;!&ndash;      <BuildCommit class="me-2" />&ndash;&gt;-->
+<!--      &lt;!&ndash;      <BuildVersion />&ndash;&gt;-->
+<!--    </div>-->
+<!--  </div>-->
+
+<!--  &lt;!&ndash;  <AppFooter />&ndash;&gt;-->
+<!--</template>-->
+
+<!--<script setup lang="ts">-->
+<!--import { useCurrentUser } from 'colada/queries/current-user'-->
+
+<!--const { isAdmin } = useCurrentUser()-->
+<!--</script>-->
---
+++ tsugini/src/types/RouterMeta.d.ts
@@ -0,0 +1,15 @@
+// This can be directly added to any of your `.ts` files like `router.ts`
+// It can also be added to a `.d.ts` file. Make sure it's included in
+// project's tsconfig.json "files"
+import 'vue-router'
+import type { UserRoles } from '@/types/UserRoles.ts'
+
+// To ensure it is treated as a module, add at least one `export` statement
+export {}
+
+declare module 'vue-router' {
+  interface RouteMeta {
+    noAuth?: boolean
+    requiresRole?: UserRoles
+  }
+}
