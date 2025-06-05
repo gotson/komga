750b4d64fbb2628cd1a0ddad383bb76ce20989db
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2025-06-05 12:50:52 +0800

    vuetify stuff

--- next-ui/src/App.vue
+++ next-ui/src/App.vue
@@ -6,7 +6,7 @@
 
 <script lang="ts" setup>
 import { useTheme } from 'vuetify'
-import {useAppStore} from '@/stores/app'
+import { useAppStore } from '@/stores/app'
 import { usePreferredDark } from '@vueuse/core'
 
 const appStore = useAppStore()
@@ -14,19 +14,21 @@ const theme = useTheme()
 const prefersDark = usePreferredDark()
 
 function updateTheme(selectedTheme: string, prefersDark: boolean) {
-  if(selectedTheme === 'system') {
+  if (selectedTheme === 'system') {
     theme.global.name.value = prefersDark ? 'dark' : 'light'
   } else {
     theme.global.name.value = selectedTheme
   }
 }
 
-watch([() => appStore.theme, prefersDark], ([selectedTheme, prefersDark]) => updateTheme(selectedTheme, prefersDark))
+watch([() => appStore.theme, prefersDark], ([selectedTheme, prefersDark]) =>
+  updateTheme(selectedTheme, prefersDark),
+)
 
 // trigger an update on startup to get the proper theme loaded
 updateTheme(appStore.theme, prefersDark.value)
 </script>
 
 <style>
-@import "styles/global.scss";
+@import 'styles/global.scss';
 </style>
