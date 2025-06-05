c44865fbdff4093dc2fd76fd4693bf492d3d5a9c
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2025-06-05 16:46:50 +0800

    tree shake icons

--- next-ui/env.d.ts
+++ next-ui/env.d.ts
@@ -1,3 +1,4 @@
 /// <reference types="vite/client" />
 /// <reference types="unplugin-vue-router/client" />
 /// <reference types="vite-plugin-vue-layouts-next/client" />
+/// <reference types="unplugin-icons/types/vue3" />
