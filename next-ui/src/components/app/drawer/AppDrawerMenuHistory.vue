879eb6107a4ab9820f5187ef61c9cced6805b583
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2025-06-05 12:50:52 +0800

    vuetify stuff

--- next-ui/src/components/app/drawer/AppDrawerMenuHistory.vue
+++ next-ui/src/components/app/drawer/AppDrawerMenuHistory.vue
@@ -1,11 +1,13 @@
 <template>
   <v-list-item
     to="/history"
-    :title="$formatMessage({
-      description: 'Drawer menu for History',
-      defaultMessage: 'History',
-      id: 'l/To3S'
-    })"
+    :title="
+      $formatMessage({
+        description: 'Drawer menu for History',
+        defaultMessage: 'History',
+        id: 'l/To3S',
+      })
+    "
     prepend-icon="mdi-clock-time-four-outline"
   />
 </template>
