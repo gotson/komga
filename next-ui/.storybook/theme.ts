e6f76bba718fefdc7011787d4e4172b29f065b49
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2025-06-12 16:30:00 +0800

    add storybook

---
+++ next-ui/.storybook/theme.ts
@@ -0,0 +1,8 @@
+import { create } from 'storybook/theming'
+
+export default create({
+  base: 'light',
+  brandTitle: 'Komga Storybook',
+  brandUrl: 'https://komga.org',
+  brandImage: '../src/assets/komga.svg',
+})
