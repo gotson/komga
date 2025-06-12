4176649ebe191764b72f2264945d4ffbb8df8e52
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2025-06-12 16:30:00 +0800

    add storybook

---
+++ next-ui/.storybook/main.ts
@@ -0,0 +1,19 @@
+import type { StorybookConfig } from '@storybook/vue3-vite'
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@chromatic-com/storybook',
+    '@storybook/addon-docs',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-a11y',
+    '@storybook/addon-vitest',
+    '@storybook/addon-themes',
+  ],
+  framework: {
+    name: '@storybook/vue3-vite',
+    options: {},
+  },
+  staticDirs: ['../public-msw'],
+}
+export default config
