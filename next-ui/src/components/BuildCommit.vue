77148e87b400ec9b94cb47d67a858205c24e09e6
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2025-06-12 16:30:00 +0800

    add storybook

--- next-ui/src/components/BuildCommit.vue
+++ next-ui/src/components/BuildCommit.vue
@@ -1,22 +1,22 @@
 <template>
-  <template v-if="commitId">
-    <v-btn
-      :prepend-icon="mdiSourceCommit"
-      variant="text"
-      color="grey"
-      size="small"
-      class="text-caption"
-      :href="'https://github.com/gotson/komga/commits/' + commitId"
-      target="_blank"
-    >
-      {{ commitId }}
-    </v-btn>
-  </template>
+  <v-btn
+    :loading="isLoading"
+    :prepend-icon="mdiSourceCommit"
+    variant="text"
+    color="grey"
+    size="small"
+    class="text-caption"
+    :href="'https://github.com/gotson/komga/commits/' + commitId"
+    target="_blank"
+  >
+    {{ commitId || $formatMessage(commonMessages.error) }}
+  </v-btn>
 </template>
 
 <script setup lang="ts">
 import mdiSourceCommit from '~icons/mdi/source-commit'
 import { useActuatorInfo } from '@/colada/queries/actuator-info'
+import { commonMessages } from '@/utils/i18n/common-messages'
 
-const { commitId } = useActuatorInfo()
+const { commitId, isLoading } = useActuatorInfo()
 </script>
