e12c91c4d0cf68e933d2072485aae23cf81e91df
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2025-06-13 17:57:53 +0800

    upgrade pinia colada and refactor tests

--- next-ui/src/colada/queries/actuator-info.test.ts
+++ next-ui/src/colada/queries/actuator-info.test.ts
@@ -1,24 +1,20 @@
-import { afterAll, afterEach, beforeAll, beforeEach, expect, test } from 'vitest'
+import { afterAll, afterEach, beforeAll, expect, test } from 'vitest'
 import { server } from '@/mocks/api/node'
 import { useActuatorInfo } from '@/colada/queries/actuator-info'
 import { createMockColada } from '@/mocks/pinia-colada'
 import { http } from 'msw'
 import { baseUrl, response401Unauthorized } from '@/mocks/api/handlers/base'
-import { VueWrapper } from '@vue/test-utils'
+import { enableAutoUnmount } from '@vue/test-utils'
 import type { ErrorCause } from '@/api/komga-client'
 
-let mock: VueWrapper
 beforeAll(() => server.listen())
-beforeEach(() => {
-  mock = createMockColada()
-})
-afterEach(() => {
-  server.resetHandlers()
-  mock.unmount()
-})
+afterEach(() => server.resetHandlers())
 afterAll(() => server.close())
 
+enableAutoUnmount(afterEach)
+
 test('when getting actuator-info then values are correct', async () => {
+  createMockColada(useActuatorInfo)
   const { buildVersion, commitId, refresh } = useActuatorInfo()
 
   await refresh()
@@ -29,6 +25,7 @@ test('when getting actuator-info then values are correct', async () => {
 test('when failing to get actuator-info then values are undefined', async () => {
   server.use(http.get(baseUrl + 'actuator/info', response401Unauthorized))
 
+  createMockColada(useActuatorInfo)
   const { buildVersion, commitId, refresh, error } = useActuatorInfo()
 
   await refresh()
--- next-ui/src/colada/queries/announcements.test.ts
+++ next-ui/src/colada/queries/announcements.test.ts
@@ -1,21 +1,17 @@
-import { afterAll, afterEach, beforeAll, beforeEach, expect, test } from 'vitest'
+import { afterAll, afterEach, beforeAll, expect, test } from 'vitest'
 import { server } from '@/mocks/api/node'
 import { createMockColada } from '@/mocks/pinia-colada'
 import { useAnnouncements } from '@/colada/queries/announcements'
-import { VueWrapper } from '@vue/test-utils'
+import { enableAutoUnmount } from '@vue/test-utils'
 
-let mock: VueWrapper
 beforeAll(() => server.listen())
-beforeEach(() => {
-  mock = createMockColada()
-})
-afterEach(() => {
-  server.resetHandlers()
-  mock.unmount()
-})
+afterEach(() => server.resetHandlers())
 afterAll(() => server.close())
 
+enableAutoUnmount(afterEach)
+
 test('when getting announcements then values are correct', async () => {
+  createMockColada(useAnnouncements)
   const { unreadCount, refresh } = useAnnouncements()
 
   await refresh()
