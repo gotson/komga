29c8a8fec210e8da5eb2e5df1ddacdd9f89fa610
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2025-06-25 17:06:19 +0800

    fix type check

--- next-ui/uno.config.ts
+++ next-ui/uno.config.ts
@@ -4,7 +4,7 @@ import { aliases } from 'vuetify/iconsets/mdi'
 
 // build the safelist from the vuetify icon aliases
 const vuetifyIcons = Object.keys(aliases).map(
-  (key) => `i-mdi:${aliases[key].toString().substring(4)}`,
+  (key) => `i-mdi:${aliases[key]!.toString().substring(4)}`,
 )
 
 export default defineConfig({
