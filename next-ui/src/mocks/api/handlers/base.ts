20c57a4f65ee403fec4b93c51eb4e17f93bbf4b6
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2025-06-25 12:01:53 +0800

    revamp tests

--- next-ui/src/mocks/api/handlers/base.ts
+++
@@ -1,6 +0,0 @@
-import { HttpResponse } from 'msw'
-
-export const baseUrl = import.meta.env.VITE_KOMGA_API_URL + '/'
-
-export const response401Unauthorized = () =>
-  HttpResponse.json({ error: 'Unauthorized' }, { status: 401 })
