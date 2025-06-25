737625562a8428f0466f92ecc1aa3febff6387d5
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2025-06-25 12:01:53 +0800

    revamp tests

--- next-ui/src/colada/queries/actuator-info.test.ts
+++ next-ui/src/colada/queries/actuator-info.test.ts
@@ -1,11 +1,11 @@
-import { afterAll, afterEach, beforeAll, expect, test } from 'vitest'
+import { afterAll, afterEach, beforeAll, describe, expect, test } from 'vitest'
 import { server } from '@/mocks/api/node'
 import { useActuatorInfo } from '@/colada/queries/actuator-info'
 import { createMockColada } from '@/mocks/pinia-colada'
-import { http } from 'msw'
-import { baseUrl, response401Unauthorized } from '@/mocks/api/handlers/base'
 import { enableAutoUnmount } from '@vue/test-utils'
 import type { ErrorCause } from '@/api/komga-client'
+import { response401Unauthorized } from '@/mocks/api/handlers'
+import { httpTyped } from '@/mocks/api/httpTyped'
 
 beforeAll(() => server.listen())
 afterEach(() => server.resetHandlers())
@@ -13,23 +13,25 @@ afterAll(() => server.close())
 
 enableAutoUnmount(afterEach)
 
-test('when getting actuator-info then values are correct', async () => {
-  createMockColada(useActuatorInfo)
-  const { buildVersion, commitId, refresh } = useActuatorInfo()
+describe('colada actuator', () => {
+  test('when getting actuator-info then values are correct', async () => {
+    createMockColada(useActuatorInfo)
+    const { buildVersion, commitId, refresh } = useActuatorInfo()
 
-  await refresh()
-  expect(buildVersion.value).toBe('9.9.9')
-  expect(commitId.value).toBe('ABC123')
-})
+    await refresh()
+    expect(buildVersion.value).toBe('1.21.2')
+    expect(commitId.value).toBe('9be980d')
+  })
 
-test('when failing to get actuator-info then values are undefined', async () => {
-  server.use(http.get(baseUrl + 'actuator/info', response401Unauthorized))
+  test('when failing to get actuator-info then values are undefined', async () => {
+    server.use(httpTyped.get('/actuator/info', response401Unauthorized))
 
-  createMockColada(useActuatorInfo)
-  const { buildVersion, commitId, refresh, error } = useActuatorInfo()
+    createMockColada(useActuatorInfo)
+    const { buildVersion, commitId, refresh, error } = useActuatorInfo()
 
-  await refresh()
-  expect(buildVersion.value).toBeUndefined()
-  expect(commitId.value).toBeUndefined()
-  expect((error.value?.cause as ErrorCause).status).toBe(401)
+    await refresh()
+    expect(buildVersion.value).toBeUndefined()
+    expect(commitId.value).toBeUndefined()
+    expect((error.value?.cause as ErrorCause).status).toBe(401)
+  })
 })
