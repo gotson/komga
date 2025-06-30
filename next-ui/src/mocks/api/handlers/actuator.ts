12db85f41282955b896c0d0e6c335e76563c75e0
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2025-06-30 11:13:25 +0800

    remove @mswjs/source

---
+++ next-ui/src/mocks/api/handlers/actuator.ts
@@ -0,0 +1,42 @@
+import { httpTyped } from '@/mocks/api/httpTyped'
+
+export const actuatorInfo = {
+  git: {
+    branch: 'master',
+    commit: {
+      id: '9be980d',
+      time: '2025-03-12T03:40:38Z',
+    },
+  },
+  build: {
+    artifact: 'komga',
+    name: 'komga',
+    version: '1.21.2',
+    group: 'komga',
+  },
+  java: {
+    version: '23.0.2',
+    vendor: {
+      name: 'Eclipse Adoptium',
+      version: 'Temurin-23.0.2+7',
+    },
+    runtime: {
+      name: 'OpenJDK Runtime Environment',
+      version: '23.0.2+7',
+    },
+    jvm: {
+      name: 'OpenJDK 64-Bit Server VM',
+      vendor: 'Eclipse Adoptium',
+      version: '23.0.2+7',
+    },
+  },
+  os: {
+    name: 'Linux',
+    version: '6.8.0-57-generic',
+    arch: 'amd64',
+  },
+}
+
+export const actuatorHandlers = [
+  httpTyped.get('/actuator/info', ({ response }) => response(200).json(actuatorInfo as never)),
+]
--- next-ui/src/mocks/api/handlers/announcements.ts
+++ next-ui/src/mocks/api/handlers/announcements.ts
@@ -1,5 +1,46 @@
 import { httpTyped } from '@/mocks/api/httpTyped'
 
+export const announcementsMixedRead = {
+  version: 'https://jsonfeed.org/version/1',
+  title: 'Announcements',
+  home_page_url: 'https://komga.org/blog',
+  description: 'Latest Komga announcements',
+  items: [
+    {
+      id: 'https://komga.org/blog/ebook-drop2',
+      url: 'https://komga.org/blog/ebook-drop2',
+      title: 'eBook drop 2',
+      summary: 'Version 1.9.0 contains the second feature drop for Ebooks support.',
+      content_html: '<p>A longer text…</p>',
+      date_modified: new Date('2023-12-15T00:00:00Z'),
+      author: {
+        name: 'gotson',
+        url: 'https://github.com/gotson',
+      },
+      tags: ['upgrade', 'komga'],
+      _komga: {
+        read: false,
+      },
+    },
+    {
+      id: 'https://komga.org/blog/ebook-support',
+      url: 'https://komga.org/blog/ebook-support',
+      title: 'eBook support',
+      summary: 'Version 1.8.0 is bringing a long awaited feature: proper eBook support!',
+      content_html: '<p>A longer text…</p>',
+      date_modified: new Date('2023-11-29T00:00:00Z'),
+      author: {
+        name: 'gotson',
+        url: 'https://github.com/gotson',
+      },
+      tags: ['upgrade', 'komga'],
+      _komga: {
+        read: true,
+      },
+    },
+  ],
+}
+
 export const announcementsAllRead = {
   version: 'https://jsonfeed.org/version/1',
   title: 'Announcements',
@@ -42,5 +83,8 @@ export const announcementsAllRead = {
 }
 
 export const announcementHandlers = [
+  httpTyped.get('/api/v1/announcements', ({ response }) =>
+    response(200).json(announcementsMixedRead),
+  ),
   httpTyped.put('/api/v1/announcements', ({ response }) => response(204).empty()),
 ]
