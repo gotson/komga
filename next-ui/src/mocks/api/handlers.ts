709044e42f91fde5e22b31338fb10fd4ab9d32f7
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2025-06-30 15:40:45 +0800

    stuff for users page

--- next-ui/src/mocks/api/handlers.ts
+++ next-ui/src/mocks/api/handlers.ts
@@ -4,6 +4,7 @@ import { releasesHandlers } from '@/mocks/api/handlers/releases'
 import { HttpResponse } from 'msw'
 import { librariesHandlers } from '@/mocks/api/handlers/libraries'
 import { referentialHandlers } from '@/mocks/api/handlers/referential'
+import { usersHandlers } from '@/mocks/api/handlers/users'
 
 export const handlers = [
   ...librariesHandlers,
@@ -11,6 +12,7 @@ export const handlers = [
   ...actuatorHandlers,
   ...announcementHandlers,
   ...releasesHandlers,
+  ...usersHandlers,
 ]
 
 export const response401Unauthorized = () =>
