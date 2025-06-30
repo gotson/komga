2eb647761dc8471c4a2226a816b1d0df5578350a
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2025-06-30 11:13:25 +0800

    remove @mswjs/source

--- next-ui/src/mocks/api/handlers.ts
+++ next-ui/src/mocks/api/handlers.ts
@@ -1,17 +1,9 @@
+import { actuatorHandlers } from '@/mocks/api/handlers/actuator'
 import { announcementHandlers } from '@/mocks/api/handlers/announcements'
 import { releasesHandlers } from '@/mocks/api/handlers/releases'
-import { fromOpenApi } from '@mswjs/source/open-api'
-import type { OpenAPIV3 } from 'openapi-types'
-import spec from '../../../../komga/docs/openapi.json'
 import { HttpResponse } from 'msw'
 
-const doc = {
-  basePath: import.meta.env.VITE_KOMGA_API_URL,
-  ...spec,
-} as unknown as OpenAPIV3.Document
-
-// manually defined handlers need to be before fromOpenApi
-export const handlers = [...announcementHandlers, ...releasesHandlers, ...(await fromOpenApi(doc))]
+export const handlers = [...actuatorHandlers, ...announcementHandlers, ...releasesHandlers]
 
 export const response401Unauthorized = () =>
   HttpResponse.json({ error: 'Unauthorized' }, { status: 401 })
