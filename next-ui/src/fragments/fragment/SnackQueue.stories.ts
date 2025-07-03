274d9e0ccc36b6fb447774adafd24bdda9e216ec
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2025-07-03 17:55:01 +0800

    storybook: moar!

---
+++ next-ui/src/fragments/fragment/SnackQueue.stories.ts
@@ -0,0 +1,96 @@
+import type { Meta, StoryObj } from '@storybook/vue3-vite'
+
+import SnackQueue from './SnackQueue.vue'
+import { useMessagesStore } from '@/stores/messages'
+
+const meta = {
+  component: SnackQueue,
+  render: (args: object) => ({
+    components: { SnackQueue },
+    setup() {
+      return { args }
+    },
+    template: '<SnackQueue />',
+  }),
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
+  },
+  args: {},
+} satisfies Meta<typeof SnackQueue>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {},
+  play: () => {
+    const messagesStore = useMessagesStore()
+    messagesStore.messages = [{ text: 'Default notification' }]
+  },
+}
+
+export const NoTimeout: Story = {
+  args: {},
+  play: () => {
+    const messagesStore = useMessagesStore()
+    messagesStore.messages = [
+      {
+        text: 'I will not timeout, click me to make me go away',
+        timeout: -1,
+        timer: false,
+      },
+    ]
+  },
+}
+
+export const Success: Story = {
+  args: {},
+  play: () => {
+    const messagesStore = useMessagesStore()
+    messagesStore.messages = [
+      {
+        text: 'Success',
+        color: 'success',
+      },
+    ]
+  },
+}
+
+export const Warning: Story = {
+  args: {},
+  play: () => {
+    const messagesStore = useMessagesStore()
+    messagesStore.messages = [
+      {
+        text: 'Warning',
+        color: 'warning',
+      },
+    ]
+  },
+}
+
+export const Error: Story = {
+  args: {},
+  play: () => {
+    const messagesStore = useMessagesStore()
+    messagesStore.messages = [
+      {
+        text: 'Error',
+        color: 'error',
+      },
+    ]
+  },
+}
+
+export const Info: Story = {
+  args: {},
+  play: () => {
+    const messagesStore = useMessagesStore()
+    messagesStore.messages = [
+      {
+        text: 'Info',
+        color: 'info',
+      },
+    ]
+  },
+}
