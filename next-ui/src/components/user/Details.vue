b6c395da45911e8a06ec802476f8f24e6db580e7
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2025-07-21 15:27:13 +0800

    account details screen

---
+++ next-ui/src/components/user/Details.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-container class="pa-0">
+    <v-row align="center">
+      <v-col cols="auto">
+        <v-avatar
+          color="primary"
+          size="x-large"
+          ><span class="text-h5 text-uppercase">{{ user.email.charAt(0) }}</span></v-avatar
+        >
+      </v-col>
+      <v-col>
+        {{ user.email }}
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <div class="d-flex ga-1 flex-wrap">
+          <v-chip
+            v-for="role in user.roles"
+            :key="role"
+            :text="$formatMessage(userRolesMessages[role as UserRoles])"
+            size="small"
+            rounded
+          />
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+import type { components } from '@/generated/openapi/komga'
+import { UserRoles, userRolesMessages } from '@/types/UserRoles'
+
+const { user } = defineProps<{
+  user: components['schemas']['UserDto']
+}>()
+</script>
--- next-ui/src/pages/account/details.vue
+++ next-ui/src/pages/account/details.vue
@@ -1,7 +1,89 @@
 <template>
-  <h1>Account Details</h1>
+  <v-empty-state
+    v-if="error"
+    icon="i-mdi:connection"
+    :title="$formatMessage(commonMessages.somethingWentWrongTitle)"
+    :text="$formatMessage(commonMessages.somethingWentWrongSubTitle)"
+  />
+
+  <template v-else-if="currentUser">
+    <UserDetails :user="currentUser" />
+    <v-btn
+      :text="
+        $formatMessage({
+          description: 'User details screen: change password button',
+          defaultMessage: 'Change password',
+          id: 'sGsWvI',
+        })
+      "
+      class="mt-8"
+      @click="changePassword()"
+      @mouseenter="dialogConfirmEdit.activator = $event.currentTarget"
+    />
+  </template>
 </template>
 
 <script lang="ts" setup>
-//
+import { useCurrentUser, useUpdateUserPassword } from '@/colada/users'
+import { commonMessages } from '@/utils/i18n/common-messages'
+import { storeToRefs } from 'pinia'
+import { useDialogsStore } from '@/stores/dialogs'
+import UserFormChangePassword from '@/components/user/form/ChangePassword.vue'
+import type { ErrorCause } from '@/api/komga-client'
+import { useMessagesStore } from '@/stores/messages'
+import { useIntl } from 'vue-intl'
+
+const intl = useIntl()
+
+const { data: currentUser, error } = useCurrentUser()
+const { mutateAsync: mutateUserPassword } = useUpdateUserPassword()
+
+const { confirmEdit: dialogConfirmEdit } = storeToRefs(useDialogsStore())
+const messagesStore = useMessagesStore()
+
+function changePassword() {
+  dialogConfirmEdit.value.dialogProps = {
+    title: intl.formatMessage(commonMessages.changePasswordDialogTitle),
+    subtitle: currentUser.value?.email,
+    maxWidth: 400,
+    closeOnSave: false,
+  }
+  dialogConfirmEdit.value.slot = {
+    component: markRaw(UserFormChangePassword),
+    props: {},
+  }
+  // password change initiated with an empty string
+  dialogConfirmEdit.value.record = ''
+  dialogConfirmEdit.value.callback = handleDialogConfirmation
+}
+
+function handleDialogConfirmation(
+  hideDialog: () => void,
+  setLoading: (isLoading: boolean) => void,
+) {
+  setLoading(true)
+
+  mutateUserPassword({
+    userId: currentUser.value!.id,
+    newPassword: dialogConfirmEdit.value.record as string,
+  })
+    ?.then(() => {
+      hideDialog()
+      messagesStore.messages.push({
+        text: intl.formatMessage({
+          description:
+            "Snackbar notification shown upon successful current user's password modification",
+          defaultMessage: 'Password changed',
+          id: '0FEy0X',
+        }),
+      })
+    })
+    .catch((error) => {
+      messagesStore.messages.push({
+        text:
+          (error?.cause as ErrorCause)?.message || intl.formatMessage(commonMessages.networkError),
+      })
+      setLoading(false)
+    })
+}
 </script>
