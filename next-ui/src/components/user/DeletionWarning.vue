b1efff2438e5ab189f7ffd9977d31d29df833368
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2025-07-22 17:23:53 +0800

    i18n notices

--- next-ui/src/components/user/DeletionWarning.vue
+++ next-ui/src/components/user/DeletionWarning.vue
@@ -4,13 +4,33 @@
     variant="tonal"
     class="mb-4"
   >
-    <div>The user account will be deleted from this server.</div>
-    <ul class="ps-8">
-      <li>The read progress for this user account will be permanently deleted.</li>
-      <li>Authentication activity for this user will be permanently deleted.</li>
-    </ul>
-    <div class="font-weight-bold mt-4">This action cannot be undone.</div>
+    <FormattedMessage :messageDescriptor="message">
+      <template #ul="Content">
+        <ul class="ps-8">
+          <component :is="Content" />
+        </ul>
+      </template>
+      <template #li="Content">
+        <li>
+          <component :is="Content" />
+        </li>
+      </template>
+      <template #b="Content">
+        <div class="font-weight-bold mt-4">
+          <component :is="Content" />
+        </div>
+      </template>
+    </FormattedMessage>
   </v-alert>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { defineMessage } from 'vue-intl'
+
+const message = defineMessage({
+  description: 'User deletion warning notice',
+  defaultMessage:
+    'The user account will be deleted from this server.<ul><li>The read progress for this user account will be permanently deleted.</li><li>Authentication activity for this user will be permanently deleted.</li></ul><b>This action cannot be undone.</b>',
+  id: 'q7gxLw',
+})
+</script>
