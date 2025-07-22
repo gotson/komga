56d83eea2c1093d3b0565d86c2027dc8436a3b77
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2025-07-22 13:57:35 +0800

    revamp user details

--- next-ui/src/components/user/Details.vue
+++ next-ui/src/components/user/Details.vue
@@ -1,36 +1,39 @@
 <template>
-  <v-container class="pa-0">
-    <v-row align="center">
-      <v-col cols="auto">
-        <v-avatar
-          color="primary"
-          size="x-large"
-          ><span class="text-h5 text-uppercase">{{ user.email.charAt(0) }}</span></v-avatar
-        >
-      </v-col>
-      <v-col>
-        {{ user.email }}
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col>
-        <div class="d-flex ga-1 flex-wrap">
-          <v-chip
-            v-for="role in user.roles"
-            :key="role"
-            :text="$formatMessage(userRolesMessages[role as UserRoles])"
-            size="small"
-            rounded
-          />
-        </div>
-      </v-col>
-    </v-row>
-  </v-container>
+  <v-card :title="user.email">
+    <template #prepend>
+      <v-avatar
+        color="primary"
+        size="x-large"
+        ><span class="text-h5 text-uppercase">{{ user.email.charAt(0) }}</span></v-avatar
+      >
+    </template>
+
+    <template #text>
+      <div class="d-flex ga-2 flex-wrap">
+        <v-chip
+          v-for="role in user.roles"
+          :key="role"
+          :text="$formatMessage(userRolesMessages[role as UserRoles])"
+          size="small"
+          rounded
+        />
+      </div>
+    </template>
+
+    <template
+      #actions
+      v-if="slots.actions"
+    >
+      <slot name="actions"></slot>
+    </template>
+  </v-card>
 </template>
 
 <script setup lang="ts">
 import type { components } from '@/generated/openapi/komga'
 import { UserRoles, userRolesMessages } from '@/types/UserRoles'
+
+const slots = useSlots()
 
 const { user } = defineProps<{
   user: components['schemas']['UserDto']
