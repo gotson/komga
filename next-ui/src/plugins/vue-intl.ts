85668239be2aa478309048ba9ab3d050d1202c04
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2025-07-23 11:58:33 +0800

    use navigator preferred languages to select locale

--- next-ui/src/plugins/vue-intl.ts
+++ next-ui/src/plugins/vue-intl.ts
@@ -1,10 +1,10 @@
 import { createIntl } from 'vue-intl'
-import { currentLocale, defaultLocale, loadLocale } from '@/utils/i18n/locale-helper'
+import { currentLocale, fallbackLocale, loadLocale } from '@/utils/i18n/locale-helper'
 
 const messages = loadLocale(currentLocale)
 
 export const vueIntl = createIntl({
   locale: currentLocale,
-  defaultLocale: defaultLocale,
+  defaultLocale: fallbackLocale,
   messages,
 })
