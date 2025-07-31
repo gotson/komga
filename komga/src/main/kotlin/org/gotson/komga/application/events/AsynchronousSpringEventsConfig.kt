a80eb59b1b0c7404de16fdfe78677cecd5954b54
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2025-07-31 15:00:28 +0800

    test: use synchronous events for tests

--- komga/src/main/kotlin/org/gotson/komga/application/events/AsynchronousSpringEventsConfig.kt
+++ komga/src/main/kotlin/org/gotson/komga/application/events/AsynchronousSpringEventsConfig.kt
@@ -2,10 +2,12 @@ package org.gotson.komga.application.events
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.context.event.ApplicationEventMulticaster
 import org.springframework.context.event.SimpleApplicationEventMulticaster
 import org.springframework.core.task.AsyncTaskExecutor
 
+@Profile("!test")
 @Configuration
 class AsynchronousSpringEventsConfig(
   private val applicationTaskExecutor: AsyncTaskExecutor,
