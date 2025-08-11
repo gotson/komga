5d49e413707a8db85c51d2d7b31584701b54dfa3
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2025-08-11 14:02:29 +0800

    chromatic modes

--- next-ui/.storybook/viewport.ts
+++ next-ui/.storybook/viewport.ts
@@ -1,37 +1,37 @@
-export const viewports = [
-  {
+export const vuetifyViewports = {
+  xs: {
     name: 'xs',
     styles: {
       width: '400px',
       height: '963px',
     },
   },
-  {
+  sm: {
     name: 'sm',
     styles: {
       width: '950px',
       height: '963px',
     },
   },
-  {
+  md: {
     name: 'md',
     styles: {
       width: '1200px',
       height: '963px',
     },
   },
-  {
+  lg: {
     name: 'lg',
     styles: {
       width: '1900px',
       height: '963px',
     },
   },
-  {
+  xl: {
     name: 'xl',
     styles: {
       width: '2550px',
       height: '963px',
     },
   },
-]
+}
