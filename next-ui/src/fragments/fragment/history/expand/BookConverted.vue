9c7458e94f12d2cb3b764cce025b70bba7838e13
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2025-08-11 11:34:58 +0800

    add history page

---
+++ next-ui/src/fragments/fragment/history/expand/BookConverted.vue
@@ -0,0 +1,16 @@
+<template>
+  <fragment-history-expand-table :rows="rows" />
+</template>
+
+<script setup lang="ts">
+import type { components } from '@/generated/openapi/komga'
+
+const { event } = defineProps<{
+  event: components['schemas']['HistoricalEventDto']
+}>()
+
+const rows = computed(() => [
+  { header: 'Before', value: event.properties['former file'] },
+  { header: 'After', value: event.properties.name },
+])
+</script>
---
+++ next-ui/src/fragments/fragment/history/expand/SeriesFolderDeleted.vue
@@ -0,0 +1,16 @@
+<template>
+  <fragment-history-expand-table :rows="rows" />
+</template>
+
+<script setup lang="ts">
+import type { components } from '@/generated/openapi/komga'
+
+const { event } = defineProps<{
+  event: components['schemas']['HistoricalEventDto']
+}>()
+
+const rows = computed(() => [
+  { header: 'Folder', value: event.properties.name },
+  { header: 'Reason', value: event.properties.reason },
+])
+</script>
