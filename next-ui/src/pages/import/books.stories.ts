b934e6145d6657a591e38a77da81319e961b198b
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2025-10-03 11:50:23 +0800

    book import

---
+++ next-ui/src/pages/import/books.stories.ts
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj } from '@storybook/vue3-vite'
+
+import ImportBooks from './books.vue'
+import DialogConfirmEdit from '@/fragments/fragment/dialog/ConfirmEdit.vue'
+import { delay, http } from 'msw'
+
+const meta = {
+  component: ImportBooks,
+  render: (args: object) => ({
+    components: { ImportBooks, DialogConfirmEdit },
+    setup() {
+      return { args }
+    },
+    template: '<ImportBooks /><DialogConfirmEdit/>',
+  }),
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
+  },
+  args: {},
+} satisfies Meta<typeof ImportBooks>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {},
+}
+
+export const Loading: Story = {
+  parameters: {
+    msw: {
+      handlers: [http.all('*', async () => await delay(2_000))],
+    },
+  },
+}
