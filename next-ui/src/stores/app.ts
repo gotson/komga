7b5bbdafce11817f54a7871fafc128bb932b5abf
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2026-01-14 16:42:41 +0800

    series page scaffolding

--- next-ui/src/stores/app.ts
+++ next-ui/src/stores/app.ts
@@ -1,6 +1,8 @@
 // Utilities
 import { defineStore } from 'pinia'
 import { useDisplay } from 'vuetify'
+import type { PresentationMode } from '@/types/libraries'
+import type { PageSize } from '@/types/page'
 
 export const useAppStore = defineStore('app', {
   state: () => ({
@@ -9,12 +11,35 @@ export const useAppStore = defineStore('app', {
     theme: 'system',
     rememberMe: false,
     importBooksPath: '',
+    browsingPageSize: 20 as PageSize,
+    /**
+     * Store the presentation mode per view.
+     * Use the getter to ensure a default value is always set.
+     */
+    presentationMode: {} as Record<string, PresentationMode>,
     // transient
     reorderLibraries: false,
   }),
+  getters: {
+    getPresentationMode: (state) => (key: string, defaultValue: PresentationMode) => {
+      return computed({
+        get: () => state.presentationMode[key] ?? (state.presentationMode[key] = defaultValue),
+        set: (value) => {
+          state.presentationMode[key] = value
+        },
+      })
+    },
+  },
   persist: {
     key: 'komga.nextui.app',
     // explicitly state which keys are stored
-    pick: ['drawer', 'theme', 'rememberMe', 'importBooksPath'],
+    pick: [
+      'drawer',
+      'theme',
+      'rememberMe',
+      'importBooksPath',
+      'browsingPageSize',
+      'presentationMode',
+    ],
   },
 })
