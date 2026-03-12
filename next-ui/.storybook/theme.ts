f82e5e7dc67dbd0493df04b62d6bb02e446e5a5c
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2026-03-12 11:07:31 +0800

    fix storybook logo in static build

--- next-ui/.storybook/theme.ts
+++ next-ui/.storybook/theme.ts
@@ -1,8 +1,9 @@
 import { create } from 'storybook/theming'
+import komgaSvg from '../src/assets/komga.svg' // Webpack/Vite processes this
 
 export default create({
   base: 'light',
   brandTitle: 'Komga Storybook',
   brandUrl: 'https://komga.org',
-  brandImage: '../src/assets/komga.svg',
+  brandImage: komgaSvg,
 })
