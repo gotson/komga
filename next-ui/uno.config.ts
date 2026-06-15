b96c736616dd3456ce9562a21cb44cf150d1b188
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2026-06-15 17:50:48 +0800

    fix unocss config for ts files

--- next-ui/uno.config.ts
+++ next-ui/uno.config.ts
@@ -1,6 +1,18 @@
 import { defineConfig, presetIcons } from 'unocss'
 
 export default defineConfig({
+  content: {
+    pipeline: {
+      include: [
+        // Retain the default file types
+        /\.(vue|svelte|[jt]sx|mdx?|astro|elm|php|phtml|html)($|\?)/,
+        // Add a glob pattern to include your js/ts files
+        'src/**/*.{js,ts}',
+      ],
+      // You can also explicitly exclude files if needed
+      // exclude: ['src/ignored-folder/**/*']
+    },
+  },
   presets: [
     presetIcons({
       // ensures proper CSS layers for icon colors
