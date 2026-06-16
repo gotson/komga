cd95a09056fa7b7ece73bd74e8b99ac4c0c7862f
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2026-06-16 15:16:46 +0800

    fix warning

--- next-ui/src/components/item/menu/ItemMenu.vue
+++ next-ui/src/components/item/menu/ItemMenu.vue
@@ -1,41 +1,43 @@
 <template>
-  <v-menu :activator="activator">
-    <v-list density="compact">
-      <v-list-item
-        v-for="(action, i) in actions"
-        :key="i"
-        v-bind="action"
-      />
+  <div>
+    <v-menu :activator="activator">
+      <v-list density="compact">
+        <v-list-item
+          v-for="(action, i) in actions"
+          :key="i"
+          v-bind="action"
+        />
 
-      <v-list-item
-        v-if="manageActions.length > 0"
-        :title="
-          $formatMessage({
-            description: 'Item menu: manage sub-menu',
-            defaultMessage: 'Manage',
-            id: 'iq1PzT',
-          })
-        "
-        append-icon="i-mdi:menu-right"
-      >
-        <v-menu
-          activator="parent"
-          open-on-click
-          open-on-hover
-          location="end"
-          submenu
+        <v-list-item
+          v-if="manageActions.length > 0"
+          :title="
+            $formatMessage({
+              description: 'Item menu: manage sub-menu',
+              defaultMessage: 'Manage',
+              id: 'iq1PzT',
+            })
+          "
+          append-icon="i-mdi:menu-right"
         >
-          <v-list density="compact">
-            <v-list-item
-              v-for="(action, i) in manageActions"
-              :key="i"
-              v-bind="action"
-            />
-          </v-list>
-        </v-menu>
-      </v-list-item>
-    </v-list>
-  </v-menu>
+          <v-menu
+            activator="parent"
+            open-on-click
+            open-on-hover
+            location="end"
+            submenu
+          >
+            <v-list density="compact">
+              <v-list-item
+                v-for="(action, i) in manageActions"
+                :key="i"
+                v-bind="action"
+              />
+            </v-list>
+          </v-menu>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </div>
 </template>
 
 <script setup lang="ts">
