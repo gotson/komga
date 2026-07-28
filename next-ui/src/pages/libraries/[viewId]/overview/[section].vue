8b2358408f369b73b2a9b03a445c28b50edbcd92
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2026-07-28 11:28:42 +0800

    implement sections in full page

--- next-ui/src/pages/libraries/[viewId]/overview/[section].vue
+++ next-ui/src/pages/libraries/[viewId]/overview/[section].vue
@@ -5,18 +5,53 @@
       :to="parentRoute"
       exact
     />
-    <v-app-bar-title :text="section" />
+    <v-app-bar-title :text="$formatMessage(overviewSectionMessages[section])" />
   </v-app-bar>
-  {{ section }}
+
+  <ItemBrowser
+    paging="scroll"
+    :items="items"
+    :presentation-mode="'grid'"
+    :has-next-page="hasNextPage"
+    @load-next-page="loadNextPage()"
+  >
+    <template #default="{ item, isSelected, preSelect, toggleSelect }">
+      <SeriesCard
+        v-if="kind === 'series'"
+        :series="item as SeriesDto"
+        stretch-poster
+        :selected="isSelected"
+        :pre-select="preSelect"
+        :width="display.xs.value ? 'auto' : appStore.gridCardWidth"
+        @selection="(_val, event) => toggleSelect(event as MouseEvent)"
+      />
+      <BookCard
+        v-else
+        :book="item as BookDto"
+        stretch-poster
+        show-series
+        :selected="isSelected"
+        :pre-select="preSelect"
+        :width="display.xs.value ? 'auto' : appStore.gridCardWidth"
+        @selection="(_val, event) => toggleSelect(event as MouseEvent)"
+      />
+    </template>
+  </ItemBrowser>
 </template>
 
 <script lang="ts" setup>
-import { OverviewSectionValues } from '@/types/OverviewSection'
+import {
+  type OverviewSection,
+  overviewSectionMessages,
+  OverviewSectionValues,
+} from '@/types/OverviewSection'
+import { useDisplay } from 'vuetify/framework'
+import { useAppStore } from '@/stores/app'
+import { useOverviewSection } from '@/composables/section'
+import { useGetLibrariesByViewId } from '@/composables/libraries'
+import { useInfiniteQuery } from '@pinia/colada'
+import type { BookDto, PageBookDto, PageSeriesDto, SeriesDto } from '@/generated/openapi'
 import type { RouteLocationRaw } from 'vue-router'
-
-const route = useRoute('/libraries/[viewId]/overview/[section]')
-const libraryViewId = route.params.viewId
-const section = route.params.section
 
 definePage({
   beforeEnter: (to) => {
@@ -27,6 +62,23 @@ definePage({
       return { name: '/libraries/[viewId]/overview', params: { viewId: params.viewId } }
     }
   },
+})
+
+const display = useDisplay()
+const appStore = useAppStore()
+
+const route = useRoute('/libraries/[viewId]/overview/[section]')
+const libraryViewId = route.params.viewId
+const { libraryIds } = useGetLibrariesByViewId(libraryViewId)
+const section = route.params.section as OverviewSection
+
+const { queryOptions, kind } = useOverviewSection(section, libraryIds)
+
+const { data, hasNextPage, loadNextPage } = useInfiniteQuery(() => queryOptions.value as never)
+
+const items = computed(() => {
+  const pages = data.value?.pages as (PageBookDto | PageSeriesDto)[] | undefined
+  return pages?.flatMap((it) => (it?.content as (BookDto | SeriesDto)[]) ?? []) ?? []
 })
 
 const parentRoute = computed<RouteLocationRaw>(() => ({
