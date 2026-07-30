6c70d3c3f371c75879fb0145bc6627e1fa8da997
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2026-07-30 16:14:20 +0800

    enum i18n

--- next-ui/src/types/OverviewSection.ts
+++ next-ui/src/types/OverviewSection.ts
@@ -22,36 +22,36 @@ export const overviewSectionMessages: Record<OverviewSection, MessageDescriptor>
   keep_reading: {
     description: 'Overview section: keep reading',
     defaultMessage: 'Keep Reading',
-    id: 'u8adLU',
+    id: 'enum.OverviewSection.keep_reading',
   },
   on_deck: {
     description: 'Overview section: on deck',
     defaultMessage: 'On Deck',
-    id: '8/5TBQ',
+    id: 'enum.OverviewSection.on_deck',
   },
   recently_released_books: {
     description: 'Overview section: recently released books',
     defaultMessage: 'Recently Released Books',
-    id: 'D9vRM7',
+    id: 'enum.OverviewSection.recently_released_books',
   },
   recently_added_books: {
     description: 'Overview section: recently added books',
     defaultMessage: 'Recently Added Books',
-    id: 'djKYIx',
+    id: 'enum.OverviewSection.recently_added_books',
   },
   recently_added_series: {
     description: 'Overview section: recently added series',
     defaultMessage: 'Recently Added Series',
-    id: '9K4zf3',
+    id: 'enum.OverviewSection.recently_added_series',
   },
   recently_updated_series: {
     description: 'Overview section: recently updated series',
     defaultMessage: 'Recently Updated Series',
-    id: 'dr+UOt',
+    id: 'enum.OverviewSection.recently_updated_series',
   },
   recently_read_books: {
     description: 'Overview section: recently read books',
     defaultMessage: 'Recently Read Books',
-    id: 'lu0M/4',
+    id: 'enum.OverviewSection.recently_read_books',
   },
 })
