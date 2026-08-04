06d3c32a028b2896fa03347beb95509ad4b51c94
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2026-08-04 17:22:12 +0800

    deps(komga): bump commons-io to 2.22.0

--- komga/build.gradle.kts
+++ komga/build.gradle.kts
@@ -62,7 +62,7 @@ dependencies {
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
   implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml")
 
-  implementation("commons-io:commons-io:2.19.0")
+  implementation("commons-io:commons-io:2.22.0")
   implementation("org.apache.commons:commons-lang3:3.18.0")
   implementation("commons-validator:commons-validator:1.10.0")
 
