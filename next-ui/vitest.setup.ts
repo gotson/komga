5b3130320e03a0d028d67d4a67510aa67ffe4659
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2026-08-04 11:36:50 +0800

    ignore some warnings when running storybook tests

--- next-ui/vitest.setup.ts
+++ next-ui/vitest.setup.ts
@@ -1,3 +1,14 @@
 import { setupOpenapiClient } from '@/api/komga-client'
 
+// ignore some warnings thrown when running Storybook in vitest
+const originalWarn = console.warn
+console.warn = (...args) => {
+  const msg = args.join(' ')
+  // Ignore the specific decodeEntities warning
+  if (msg.includes('decodeEntities option is passed but will be ignored')) {
+    return
+  }
+  originalWarn(...args)
+}
+
 setupOpenapiClient()
