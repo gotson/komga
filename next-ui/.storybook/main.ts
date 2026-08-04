721cfa77475f935be4b0934b29438a51b30e1ed5
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2026-08-04 11:37:45 +0800

    fix storybook warnings

--- next-ui/.storybook/main.ts
+++ next-ui/.storybook/main.ts
@@ -12,7 +12,9 @@ const config: StorybookConfig = {
   ],
   framework: {
     name: '@storybook/vue3-vite',
-    options: {},
+    options: {
+      docgen: 'vue-component-meta',
+    },
   },
   staticDirs: ['../public', '../public-msw'],
 }
