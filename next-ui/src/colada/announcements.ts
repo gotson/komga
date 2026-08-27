1d4e933616a93d4963a7854537ca7c9b8b798c31
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2026-08-27 14:48:24 +0800

    fix(nextui): don't try to retrieve announcements if the user is not admin

--- next-ui/src/colada/announcements.ts
+++ next-ui/src/colada/announcements.ts
@@ -1,17 +1,21 @@
 import { defineMutation, defineQuery, useMutation, useQuery, useQueryCache } from '@pinia/colada'
 import { komgaGetAnnouncements, komgaMarkAnnouncementsRead } from '@/generated/openapi'
+import { useCurrentUser } from '@/colada/users'
 
 export const QUERY_KEYS_ANNOUNCEMENTS = {
   root: ['announcements'] as const,
 }
 
 export const useAnnouncements = defineQuery(() => {
+  const { isAdmin } = useCurrentUser()
+
   const { data, ...rest } = useQuery({
     key: () => QUERY_KEYS_ANNOUNCEMENTS.root,
     query: () => komgaGetAnnouncements(),
     // 1 hour
     staleTime: 60 * 60 * 1000,
     gcTime: false,
+    enabled: () => isAdmin.value,
   })
 
   const unreadCount = computed(
