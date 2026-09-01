6bf537526a5fcd34aa1f5295b091e193664dad80
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2026-09-01 14:37:51 +0800

    feat(nextui): display error count on tabs in collection edit dialog

--- next-ui/src/components/collection/form/General.vue
+++ next-ui/src/components/collection/form/General.vue
@@ -3,6 +3,7 @@
     <v-row>
       <v-col>
         <v-text-field
+          ref="fieldNameRef"
           v-model="model.name"
           :rules="[rules.required()]"
           :label="
@@ -57,10 +58,22 @@
 <script setup lang="ts">
 import { useRules } from 'vuetify/labs/rules'
 import type { CollectionUpdateDto } from '@/generated/openapi'
+import { VTextField } from 'vuetify/components'
 
 const rules = useRules()
+
+const fieldNameRef = ref<InstanceType<typeof VTextField> | null>(null)
 
 type CollectionUpdateGeneral = Pick<CollectionUpdateDto, 'name' | 'ordered'>
 
 const model = defineModel<CollectionUpdateGeneral>({ required: true })
+
+const emit = defineEmits<{
+  'update:errorCount': [errorCount: number]
+}>()
+
+watch([() => fieldNameRef.value?.isValid], ([nameValid]) => {
+  const fields = [nameValid]
+  emit('update:errorCount', fields.filter((it) => it === false).length)
+})
 </script>
--- next-ui/src/composables/collection/useEditCollectionDialog.ts
+++ next-ui/src/composables/collection/useEditCollectionDialog.ts
@@ -32,9 +32,6 @@ export function useEditCollectionDialog() {
       }),
       subtitle: collection.name,
       maxWidth: 900,
-      cardTextProps: {
-        class: 'px-0',
-      },
       closeOnSave: false,
       scrollable: true,
       fullscreen: display.xs.value,
