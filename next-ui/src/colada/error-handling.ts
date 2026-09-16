fe215e4d26c6ce18fd348b0e9680fecf5130f13d
Gauthier Roebroeck <gauthier.roebroeck@gmail.com>
2026-09-16 11:25:45 +0800

    refactor(nextui): use querycache instead of reactive queries in global error handler

--- next-ui/src/colada/error-handling.ts
+++ next-ui/src/colada/error-handling.ts
@@ -1,6 +1,6 @@
-import type { UseQueryEntry } from '@pinia/colada'
+import { useQueryCache, type UseQueryEntry } from '@pinia/colada'
 import { isApiErrorWithCause } from '@/api/komga-client'
-import { useCurrentUser } from '@/colada/users'
+import { currentUserQuery } from '@/colada/users'
 import { logger } from '@/services/logtape'
 
 export const authErrorEvent = ref(false)
@@ -13,9 +13,12 @@ export async function globalErrorHandler(error: unknown, entry: UseQueryEntry<un
     entry.meta?.no401handling !== true
   ) {
     logger.debug('Error 401, refetch current user')
-    const { refetch, isAuthenticated } = useCurrentUser()
-    await refetch()
-    if (!isAuthenticated.value) {
+
+    const queryCache = useQueryCache()
+    const state = await queryCache.fetch(queryCache.ensure(currentUserQuery))
+    const isAuthenticated = !!state.data && !state.error
+
+    if (!isAuthenticated) {
       authErrorEvent.value = true
     }
   }
